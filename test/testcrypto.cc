b3b7271bb2a0f43ba914bff4046408d6a8cac2f4
Fei Huang <felix.fei.huang@gmail.com>
2011-03-25 02:09:45 -0400

    testcrypto completed except for testing encrytion and decryption

--- test/testcrypto.cc
+++ test/testcrypto.cc
@@ -1,7 +1,7 @@
 // test/testcrypto.cc
 // Test /libdissent/crypto.{hpp cc}
-//  
-// Author: Fei Huang <felix.fei.huang _AT_ gmail *DOT* com>
+// 
+// Author: Fei Huang <felix.fei.huang@gmail.com>
 
 /* ====================================================================
  * Dissent: Accountable Group Anonymity
@@ -26,6 +26,8 @@
  */
 
 #include <QtTest/QtTest>
+#include <QSharedPointer>
+#include <iostream>
 
 #include "../libdissent/crypto.hpp"
 
@@ -35,16 +37,111 @@ class TestCrypto: public QObject {
   Q_OBJECT
 
  private slots:
-  void TestSingletonImplementation(); 
+  void initTestCase();
+  void cleanupTestCase();
+  void TestSingletonImplementation();
+  void TestKeyPairGenerationAndChecking();
+  void TestKeySerialization();
+  void TestEncryptAndDecrypt();
+ // void TestEncryptAndDecrypt_data();
+  void TestSignAndVerify();
+  void TestHash();
+ private:
+  Crypto *crypto_;
+  int key_length_;
+  QScopedPointer<PrivateKey> private_key_;
+  QScopedPointer<PublicKey> public_key_;
 };
 
+void TestCrypto::initTestCase() {
+  crypto_ = Crypto::GetInstance();
+  key_length_ = 2048;
+}
+
+void TestCrypto::cleanupTestCase() {
+  
+}
+
 void TestCrypto::TestSingletonImplementation() {
-  Crypto *first = Crypto::GetInstance();
-  Crypto *second = Crypto::GetInstance();
-  QCOMPARE(first == second, true);    
+  Crypto *another = Crypto::GetInstance();
+  QCOMPARE(crypto_ == another, true);    
+}
+
+void TestCrypto::TestKeyPairGenerationAndChecking() {
+  private_key_.reset(crypto_->GenerateKey(key_length_));
+  public_key_.reset(new PublicKey(private_key_->toPublicKey().toRSA()));
+  QCOMPARE(crypto_->CheckKeyPair(*private_key_, *public_key_), true);
+}
+
+void TestCrypto::TestKeySerialization() {
+  QByteArray public_key_buf;
+  QByteArray private_key_buf;
+  QCOMPARE(crypto_->SerializePublicKey(*public_key_, &public_key_buf), true);
+  QEXPECT_FAIL("", "return false in the implementation.", Continue);
+  QCOMPARE(crypto_->SerializePrivateKey(*private_key_, &private_key_buf), true);
+
+  QScopedPointer<PublicKey> public_key_from_buf(
+                              crypto_->DeserializePublicKey(public_key_buf));
+  QScopedPointer<PrivateKey> private_key_from_buf(
+                              crypto_->DeserializePrivateKey(private_key_buf));
+  QCOMPARE(public_key_ == public_key_from_buf, true);
+  QCOMPARE(private_key_ == private_key_from_buf, true);
+}
+
+void TestCrypto::TestEncryptAndDecrypt() {
+ // QFETCH(PublicKey *, key);
+ // QFETCH(QByteArray, msg);
+ // QFETCH(QSharedPointer<QByteArray>, ctext);
+ // QFETCH(QSharedPointer<QByteArray>, randomness);
+
+ // QCOMPARE(crypto_->Encrypt(key, msg, ctext.data(), randomness.data()), true);
+ //QByteArray msg("GGG");
+ //QByteArray ctext;
+ //QCOMPARE(crypto_->Encrypt(public_key_.data(), msg, 
+ //         &ctext, NULL), true);
+}
+/*
+void TestCrypto::TestEncryptAndDecrypt_data() {
+  QTest::addColumn<PublicKey *>("key");
+  QTest::addColumn<QByteArray>("msg");
+  QTest::addColumn<QSharedPointer<QByteArray> >("ctext");
+  QTest::addColumn<QSharedPointer<QByteArray> >("randomness");
+
+  QByteArray msg("Hello, world!");
+
+  QTest::newRow("no randomness") << public_key_.data() << msg 
+                                 << QSharedPointer<QByteArray>(new QByteArray())
+                                 << QSharedPointer<QByteArray>(NULL);
+  QTest::newRow("want to know") << public_key_.data() << msg 
+                                << QSharedPointer<QByteArray>(new QByteArray()) 
+                                << QSharedPointer<QByteArray>(new QByteArray());
+  //QByteArray *arr = new QByteArray()
+  //QTest::newRow("known randomness") << public_key_ << msg << new QByteArray()
+}
+*/
+
+void TestCrypto::TestSignAndVerify() {
+  QByteArray msg("Hello, world!");
+  QByteArray signature;
+
+  QCOMPARE(crypto_->Sign(private_key_.data(), msg, &signature), true);
+  QCOMPARE(crypto_->Verify(public_key_.data(), msg, signature), true);
+}
+
+void TestCrypto::TestHash() {
+  QList<QByteArray> msgs;
+  QByteArray hash;
+
+  msgs.append(QByteArray("Hello"));
+  msgs.append(QByteArray(", "));
+  msgs.append(QByteArray("world!"));
+  QCOMPARE(crypto_->Hash(msgs, &hash), true);
 }
 
 }
+
+Q_DECLARE_METATYPE(Dissent::PublicKey *)
+Q_DECLARE_METATYPE(QSharedPointer<QByteArray>)
 
 QTEST_MAIN(Dissent::TestCrypto)
 #include "testcrypto.moc"
