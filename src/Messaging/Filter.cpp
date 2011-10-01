d4d36db84afc0c60b84fd9f0581ce23d2abba592
David Wolinsky <davidiw@ufl.edu>
2011-09-30 22:16:25 -0400

    [Messaging] Fixed Filter.cpp compilation warning

--- src/Messaging/Filter.cpp
+++ src/Messaging/Filter.cpp
@@ -2,7 +2,7 @@
 
 namespace Dissent {
 namespace Messaging {
-  inline void Filter::HandleData(const QByteArray &data, ISender *from)
+  inline void Filter::HandleData(const QByteArray &data, ISender *)
   {
     PushData(data, this);
   }
