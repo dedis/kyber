e48d793f3790f40ddbf464e25aeb5179461139a8
David Wolinsky <davidiw@ufl.edu>
2011-11-26 22:48:52 -0500

    [Transports] No point in printing out delay

--- src/Transports/BufferEdgeListener.cpp
+++ src/Transports/BufferEdgeListener.cpp
@@ -85,7 +85,6 @@ namespace Transports {
     }
 
     int delay = Dissent::Utils::Random::GetInstance().GetInt(10, 50);
-    qWarning() << "Delay:" << delay;
     BufferEdge *local_edge(new BufferEdge(GetAddress(), remote_el->GetAddress(), true, delay));
     BufferEdge *remote_edge(new BufferEdge(remote_el->GetAddress(), GetAddress(), false, delay));
 
