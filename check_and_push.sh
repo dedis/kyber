c854d2cd1d8c77395a5c41d42d1a1236f2e526c9
David Wolinsky <davidiw@ufl.edu>
2011-11-30 22:55:26 -0500

    Added a fancy little script that does a sanity check, pushes an update to the main repository, and updates documentation as necessary

---
+++ check_and_push.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+github_repo=github
+wd=$PWD
+rand=$RANDOM
+git clone . /tmp/dissent.$rand
+cd /tmp/dissent.$rand
+
+function cleanup {
+  echo $1
+  cd $wd
+  rm -rf /tmp/dissent.$rand
+}
+
+qmake test.pro
+if test $? -ne 0; then
+  cleanup "Error qmake test.pro"
+  exit 1
+fi
+
+make -j8
+if test $? -ne 0; then
+  cleanup "Error make"
+  exit 1
+fi
+
+./test --gtest_catch_exceptions=0
+if test $? -ne 0; then
+  cleanup "Error running test"
+  exit 1
+fi
+
+qmake console.pro
+if test $? -ne 0; then
+  cleanup "Error qmake console.pro"
+  exit 1
+fi
+
+make
+if test $? -ne 0; then
+  cleanup "Error make console.pro"
+  exit 1
+fi
+
+cd $wd
+git push $github_repo master
+if ! test -d $wd/docs/html; then
+  cleanup "All done!"
+  exit 0
+fi
+
+cd /tmp/dissent.$rand
+doxygen dissent.doxy >& /dev/null
+rm -rf $wd/docs/html/*
+cp -axf docs/html/* $wd/docs/html
+cd $wd/docs/html
+git add .
+git commit -m "documentation update"
+git push $github_repo gh-pages
+cleanup
