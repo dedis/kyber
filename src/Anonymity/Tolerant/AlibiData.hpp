franklinsxx/Dissent

[Anonymity] Introducing TolerantBulkRound

src/Anonymity/Blame/AlibiData.hpp
@@ -1,0 +1,164 @@
+#ifndef DISSENT_ANONYMITY_BLAME_ALIBI_DATA_H_GUARD
+#define DISSENT_ANONYMITY_BLAME_ALIBI_DATA_H_GUARD
+
+#include <QBitArray>
+#include <QByteArray>
+#include <QHash>
+#include <QPair>
+#include <QVector>
+
+#include "Accusation.hpp"
+
+namespace Dissent {
+namespace Anonymity {
+namespace Blame {
+
+  /**
+   * AlibiData holds all of the byte arrays that this node XORd
+   * together to form its output message in every slot. By recording
+   * which slots are corrupted at any time, AlibiData can periodically
+   * clear the message history to save space.
+   */
+  class AlibiData {
+
+    public:
+
+      /**
+       * Data about the slot
+       */
+      struct slot_data {
+        /** 
+         * The number of random bytes generated using the 
+         * RNG in all previous phases up to the start of
+         * this phase
+         */
+        uint phase_rng_byte_idx;
+
+        /** 
+         * The number of random bytes generated using the 
+         * RNG in this phase up to the start of this slot
+         */
+        uint slot_rng_byte_idx;
+
+        /**
+         * The byte arrays XORd together to produce the node's
+         * output for the given slot
+         */
+        QVector<QByteArray> xor_messages;
+      };
+
+      /** 
+       * Constructor. 
+       * @param number of slots (i.e., number of users)
+       * @param number of XOR components. For users, this is the
+       * number of servers. For servers, this is the number of users.
+       */
+      AlibiData(uint n_slots, uint n_members);
+
+      /**
+       * Store the number of bytes generated with the RNG up to the
+       * start of this phase and slot
+       * @param number of bytes generated so far
+       */
+      void StorePhaseRngByteIndex(uint byte_length);
+
+      /**
+       * Store an XOR component sent by this node in the given slot
+       * @param phase index
+       * @param slot index
+       * @param member whose shared secret was used to generate this byte array
+       * @param the byte array
+       */
+      void StoreMessage(uint phase, uint slot, uint member, const QByteArray &message);
+
+      /**
+       * Get a serialized alibi proving this node's innocence in the given slot
+       * @param slot index
+       * @param accusation describing the bit position for which to produce
+       *        an alibi
+       */
+      QByteArray GetAlibiBytes(uint slot, Accusation &acc);
+
+      /**
+       * Get a serlialized alibi proving this node's innocence in the given slot
+       * @param phase index
+       * @param corrupted slot index
+       * @param byte index within the corrupted slot
+       * @param bit index within the corrupted byte
+       */
+      QByteArray GetAlibiBytes(uint phase, uint slot, uint byte, ushort bit);
+
+      /**
+       * Indicate that the next transmission phase is starting. This allows
+       * AlibiData to allocate new memory for the next phase and to clear
+       * unneeded byte arrays where possible.
+       */
+      void NextPhase();
+
+      /**
+       * Mark that a message slot has been corrupted. This tells AlibiData
+       * to save byte arrays from this and future phases.
+       * @param slot index that was corrupted
+       */
+      void MarkSlotCorrupted(uint slot);
+
+      /**
+       * Mark that a slot is no longer corrupted. This tells AlibiData
+       * to stop saving old byte arrays from this slot.
+       * @param slot index of slot that is no longer corrupted
+       */
+      void MarkSlotBlameFinished(uint slot);
+
+      /**
+       * Get the nubmer of RNG bytes generated before the start of this slot
+       * @param phase index
+       * @param slot index
+       */
+      uint GetSlotRngByteOffset(uint phase, uint slot) const;
+
+      /**
+       * Length (in bytes) of a serialized alibi
+       * @param the number of XOR components covered by this alibi
+       */
+      static uint ExpectedAlibiLength(uint members);
+
+      /**
+       * Un-seralize an alibi
+       * @param input bytearray 
+       * @param byte offset at which to start reading
+       * @param the number of members that this alibi covers
+       */
+      static QBitArray AlibiBitsFromBytes(QByteArray &input, uint offset, uint members);
+
+    private:
+
+      /**
+       * Which slots are still awaiting blame
+       */
+      QBitArray _corrupted_slots;
+
+      /** 
+       * Number of message slots
+       */
+      const uint _n_slots;
+
+      /**
+       * Number of XOR components to store for each message slot
+       */
+      const uint _n_members;
+
+      /** 
+       * Vector of data[slot][phase] => slot data
+       */
+      QVector<QHash<uint, struct slot_data> > _data;
+
+      bool _phase_rng_byte_initialized;
+      uint _phase_rng_byte_idx;
+
+
+  };
+}
+}
+}
+
+#endif

src/Anonymity/Blame/Conflict.hpp
@@ -1,0 +1,47 @@
+#ifndef DISSENT_ANONYMITY_CONFLICT_H_GUARD
+#define DISSENT_ANONYMITY_CONFLICT_H_GUARD
+
+#include <QString>
+
+namespace Dissent {
+namespace Anonymity {
+namespace Blame {
+  
+  class Conflict {
+
+    public:
+      Conflict(uint slot_idx,
+          uint user_idx, bool user_bit,
+          uint server_idx, bool server_bit) :
+        _slot_idx(slot_idx),
+        _user_idx(user_idx),
+        _user_bit(user_bit),
+        _server_idx(server_idx),
+        _server_bit(server_bit) {}
+
+      inline uint GetSlotIndex() const { return _slot_idx; }
+
+      inline uint GetUserIndex() const { return _user_idx; }
+
+      inline bool GetUserBit() const { return _user_bit; }
+
+      inline uint GetServerIndex() const { return _server_idx; }
+
+      inline bool GetServerBit() const { return _server_bit; }
+
+    private:
+    
+      uint _slot_idx;
+
+      uint _user_idx;
+      bool _user_bit;
+
+      uint _server_idx;
+      bool _server_bit;
+
+  };
+}
+} 
+}
+
+#endif

src/Anonymity/Blame/MessageHistory.hpp
@@ -1,0 +1,112 @@
+#ifndef DISSENT_ANONYMITY_BLAME_MESSAGE_HISTORY_H_GUARD
+#define DISSENT_ANONYMITY_BLAME_MESSAGE_HISTORY_H_GUARD
+
+#include <QBitArray>
+#include <QByteArray>
+#include <QHash>
+#include <QVector>
+
+#include "Accusation.hpp"
+
+namespace Dissent {
+namespace Anonymity {
+namespace Blame {
+
+  /**
+   * MessageHistory holds a record of data messages received
+   * by a node. The history clears messages that are no longer
+   * needed at the start of every phase.
+   */
+  class MessageHistory {
+
+    public:
+
+      /** 
+       * Constructor
+       * @param number of users
+       * @param number of servers
+       */
+      MessageHistory(uint num_users, uint num_servers);
+
+      /**
+       * Add a user's message to the history
+       * @param phase in which the message was sent
+       * @param slot in which the message was sent
+       * @param index of the member who sent the message
+       * @param message bytes
+       */
+      void AddUserMessage(uint phase, uint slot, uint member, const QByteArray &message);
+
+      /**
+       * Add a servers's message to the history
+       * @param phase in which the message was sent
+       * @param slot in which the message was sent
+       * @param index of the member who sent the message
+       * @param message bytes
+       */
+      void AddServerMessage(uint phase, uint slot, uint member, const QByteArray &message);
+
+      /**
+       * Get the bit that a user sent in the position defined
+       * by an accusation
+       * @param slot for which to get the bit
+       * @param index of the user whose bit should be returned
+       * @param accusation describing the location of the corrupted bit
+       */
+      bool GetUserOutputBit(uint slot, uint user_idx, const Accusation &acc) const;
+
+      /**
+       * Get the bit that a server sent in the position defined
+       * by an accusation
+       * @param slot for which to get the bit
+       * @param index of the user whose bit should be returned
+       * @param accusation describing the location of the corrupted bit
+       */
+      bool GetServerOutputBit(uint slot, uint server_idx, const Accusation &acc) const;
+
+      /**
+       * Inform the history that a new message transmission phase has started.
+       * This allows the history to clear out unneeded messages.
+       */
+      void NextPhase();
+
+      /**
+       * Mark a message slot as corrupted. This tells the history to start
+       * saving all messages sent in this slot so that they can be used
+       * as evidence in the blame sub-protocol.
+       */
+      void MarkSlotCorrupted(uint slot);
+
+      /**
+       * Mark that the blame sub-protocol has completed for this message slot 
+       * or that the slot is no longer corrupted. This allows the history
+       * to resume clearing messages for this slot.
+       */
+      void MarkSlotBlameFinished(uint slot);
+
+    private:
+
+      /**
+       * A bitmask describing which slots are corrupted
+       */
+      QBitArray _corrupted_slots;
+
+      /**
+       * Data structures holding the messages
+       * _data[slot][phase][member] => message
+       */
+      QVector<QHash<uint,QVector<QByteArray> > > _user_data;
+      QVector<QHash<uint,QVector<QByteArray> > > _server_data;
+
+      /**
+       * The number of users and servers
+       */
+      const uint _num_users;
+      const uint _num_servers;
+
+  };
+}
+}
+}
+
+#endif

src/Dissent.hpp
@@ -5,13 +5,20 @@
 #include "Anonymity/Credentials.hpp"
 #include "Anonymity/Group.hpp"
 #include "Anonymity/Log.hpp"
+#include "Anonymity/MessageRandomizer.hpp"
 #include "Anonymity/NullRound.hpp"
 #include "Anonymity/RepeatingBulkRound.hpp"
 #include "Anonymity/Round.hpp"
 #include "Anonymity/Session.hpp"
 #include "Anonymity/SessionManager.hpp"
 #include "Anonymity/ShuffleRound.hpp"
+#include "Anonymity/TolerantBulkRound.hpp"
 #include "Anonymity/TrustedBulkRound.hpp"
+#include "Anonymity/Blame/Accusation.hpp"
+#include "Anonymity/Blame/AlibiData.hpp"
+#include "Anonymity/Blame/BlameMatrix.hpp"
+#include "Anonymity/Blame/Conflict.hpp"
+#include "Anonymity/Blame/MessageHistory.hpp"
 
 #include "Applications/CommandLine.hpp"
 #include "Applications/ConsoleSink.hpp"

dedis/kyber

src/Anonymity/Tolerant/AlibiData.hpp
@@ -1,0 +1,164 @@
+#ifndef DISSENT_ANONYMITY_TOLERANT_ALIBI_DATA_H_GUARD
+#define DISSENT_ANONYMITY_TOLERANT_ALIBI_DATA_H_GUARD
+
+#include <QBitArray>
+#include <QByteArray>
+#include <QHash>
+#include <QPair>
+#include <QVector>
+
+#include "Accusation.hpp"
+
+namespace Dissent {
+namespace Anonymity {
+namespace Tolerant {
+
+  /**
+   * AlibiData holds all of the byte arrays that this node XORd
+   * together to form its output message in every slot. By recording
+   * which slots are corrupted at any time, AlibiData can periodically
+   * clear the message history to save space.
+   */
+  class AlibiData {
+
+    public:
+
+      /**
+       * Data about the slot
+       */
+      struct slot_data {
+        /** 
+         * The number of random bytes generated using the 
+         * RNG in all previous phases up to the start of
+         * this phase
+         */
+        uint phase_rng_byte_idx;
+
+        /** 
+         * The number of random bytes generated using the 
+         * RNG in this phase up to the start of this slot
+         */
+        uint slot_rng_byte_idx;
+
+        /**
+         * The byte arrays XORd together to produce the node's
+         * output for the given slot
+         */
+        QVector<QByteArray> xor_messages;
+      };
+
+      /** 
+       * Constructor. 
+       * @param number of slots (i.e., number of users)
+       * @param number of XOR components. For users, this is the
+       * number of servers. For servers, this is the number of users.
+       */
+      AlibiData(uint n_slots, uint n_members);
+
+      /**
+       * Store the number of bytes generated with the RNG up to the
+       * start of this phase and slot
+       * @param number of bytes generated so far
+       */
+      void StorePhaseRngByteIndex(uint byte_length);
+
+      /**
+       * Store an XOR component sent by this node in the given slot
+       * @param phase index
+       * @param slot index
+       * @param member whose shared secret was used to generate this byte array
+       * @param the byte array
+       */
+      void StoreMessage(uint phase, uint slot, uint member, const QByteArray &message);
+
+      /**
+       * Get a serialized alibi proving this node's innocence in the given slot
+       * @param slot index
+       * @param accusation describing the bit position for which to produce
+       *        an alibi
+       */
+      QByteArray GetAlibiBytes(uint slot, Accusation &acc);
+
+      /**
+       * Get a serlialized alibi proving this node's innocence in the given slot
+       * @param phase index
+       * @param corrupted slot index
+       * @param byte index within the corrupted slot
+       * @param bit index within the corrupted byte
+       */
+      QByteArray GetAlibiBytes(uint phase, uint slot, uint byte, ushort bit);
+
+      /**
+       * Indicate that the next transmission phase is starting. This allows
+       * AlibiData to allocate new memory for the next phase and to clear
+       * unneeded byte arrays where possible.
+       */
+      void NextPhase();
+
+      /**
+       * Mark that a message slot has been corrupted. This tells AlibiData
+       * to save byte arrays from this and future phases.
+       * @param slot index that was corrupted
+       */
+      void MarkSlotCorrupted(uint slot);
+
+      /**
+       * Mark that a slot is no longer corrupted. This tells AlibiData
+       * to stop saving old byte arrays from this slot.
+       * @param slot index of slot that is no longer corrupted
+       */
+      void MarkSlotBlameFinished(uint slot);
+
+      /**
+       * Get the nubmer of RNG bytes generated before the start of this slot
+       * @param phase index
+       * @param slot index
+       */
+      uint GetSlotRngByteOffset(uint phase, uint slot) const;
+
+      /**
+       * Length (in bytes) of a serialized alibi
+       * @param the number of XOR components covered by this alibi
+       */
+      static uint ExpectedAlibiLength(uint members);
+
+      /**
+       * Un-seralize an alibi
+       * @param input bytearray 
+       * @param byte offset at which to start reading
+       * @param the number of members that this alibi covers
+       */
+      static QBitArray AlibiBitsFromBytes(QByteArray &input, uint offset, uint members);
+
+    private:
+
+      /**
+       * Which slots are still awaiting blame
+       */
+      QBitArray _corrupted_slots;
+
+      /** 
+       * Number of message slots
+       */
+      const uint _n_slots;
+
+      /**
+       * Number of XOR components to store for each message slot
+       */
+      const uint _n_members;
+
+      /** 
+       * Vector of data[slot][phase] => slot data
+       */
+      QVector<QHash<uint, struct slot_data> > _data;
+
+      bool _phase_rng_byte_initialized;
+      uint _phase_rng_byte_idx;
+
+
+  };
+}
+}
+}
+
+#endif

src/Anonymity/Tolerant/Conflict.hpp
@@ -1,0 +1,47 @@
+#ifndef DISSENT_ANONYMITY_TOLERANT_CONFLICT_H_GUARD
+#define DISSENT_ANONYMITY_TOLERANT_CONFLICT_H_GUARD
+
+#include <QString>
+
+namespace Dissent {
+namespace Anonymity {
+namespace Tolerant {
+  
+  class Conflict {
+
+    public:
+      Conflict(uint slot_idx,
+          uint user_idx, bool user_bit,
+          uint server_idx, bool server_bit) :
+        _slot_idx(slot_idx),
+        _user_idx(user_idx),
+        _user_bit(user_bit),
+        _server_idx(server_idx),
+        _server_bit(server_bit) {}
+
+      inline uint GetSlotIndex() const { return _slot_idx; }
+
+      inline uint GetUserIndex() const { return _user_idx; }
+
+      inline bool GetUserBit() const { return _user_bit; }
+
+      inline uint GetServerIndex() const { return _server_idx; }
+
+      inline bool GetServerBit() const { return _server_bit; }
+
+    private:
+    
+      uint _slot_idx;
+
+      uint _user_idx;
+      bool _user_bit;
+
+      uint _server_idx;
+      bool _server_bit;
+
+  };
+}
+} 
+}
+
+#endif

src/Anonymity/Tolerant/MessageHistory.hpp
@@ -1,0 +1,112 @@
+#ifndef DISSENT_ANONYMITY_TOLERANT_MESSAGE_HISTORY_H_GUARD
+#define DISSENT_ANONYMITY_TOLERANT_MESSAGE_HISTORY_H_GUARD
+
+#include <QBitArray>
+#include <QByteArray>
+#include <QHash>
+#include <QVector>
+
+#include "Accusation.hpp"
+
+namespace Dissent {
+namespace Anonymity {
+namespace Tolerant {
+
+  /**
+   * MessageHistory holds a record of data messages received
+   * by a node. The history clears messages that are no longer
+   * needed at the start of every phase.
+   */
+  class MessageHistory {
+
+    public:
+
+      /** 
+       * Constructor
+       * @param number of users
+       * @param number of servers
+       */
+      MessageHistory(uint num_users, uint num_servers);
+
+      /**
+       * Add a user's message to the history
+       * @param phase in which the message was sent
+       * @param slot in which the message was sent
+       * @param index of the member who sent the message
+       * @param message bytes
+       */
+      void AddUserMessage(uint phase, uint slot, uint member, const QByteArray &message);
+
+      /**
+       * Add a servers's message to the history
+       * @param phase in which the message was sent
+       * @param slot in which the message was sent
+       * @param index of the member who sent the message
+       * @param message bytes
+       */
+      void AddServerMessage(uint phase, uint slot, uint member, const QByteArray &message);
+
+      /**
+       * Get the bit that a user sent in the position defined
+       * by an accusation
+       * @param slot for which to get the bit
+       * @param index of the user whose bit should be returned
+       * @param accusation describing the location of the corrupted bit
+       */
+      bool GetUserOutputBit(uint slot, uint user_idx, const Accusation &acc) const;
+
+      /**
+       * Get the bit that a server sent in the position defined
+       * by an accusation
+       * @param slot for which to get the bit
+       * @param index of the user whose bit should be returned
+       * @param accusation describing the location of the corrupted bit
+       */
+      bool GetServerOutputBit(uint slot, uint server_idx, const Accusation &acc) const;
+
+      /**
+       * Inform the history that a new message transmission phase has started.
+       * This allows the history to clear out unneeded messages.
+       */
+      void NextPhase();
+
+      /**
+       * Mark a message slot as corrupted. This tells the history to start
+       * saving all messages sent in this slot so that they can be used
+       * as evidence in the blame sub-protocol.
+       */
+      void MarkSlotCorrupted(uint slot);
+
+      /**
+       * Mark that the blame sub-protocol has completed for this message slot 
+       * or that the slot is no longer corrupted. This allows the history
+       * to resume clearing messages for this slot.
+       */
+      void MarkSlotBlameFinished(uint slot);
+
+    private:
+
+      /**
+       * A bitmask describing which slots are corrupted
+       */
+      QBitArray _corrupted_slots;
+
+      /**
+       * Data structures holding the messages
+       * _data[slot][phase][member] => message
+       */
+      QVector<QHash<uint,QVector<QByteArray> > > _user_data;
+      QVector<QHash<uint,QVector<QByteArray> > > _server_data;
+
+      /**
+       * The number of users and servers
+       */
+      const uint _num_users;
+      const uint _num_servers;
+
+  };
+}
+}
+}
+
+#endif

src/Dissent.hpp
@@ -5,6 +5,7 @@
 #include "Anonymity/Credentials.hpp"
 #include "Anonymity/Group.hpp"
 #include "Anonymity/Log.hpp"
+#include "Anonymity/MessageRandomizer.hpp"
 #include "Anonymity/NullRound.hpp"
 #include "Anonymity/RepeatingBulkRound.hpp"
 #include "Anonymity/Round.hpp"
@@ -12,6 +13,12 @@
 #include "Anonymity/SessionManager.hpp"
 #include "Anonymity/ShuffleRound.hpp"
 #include "Anonymity/TrustedBulkRound.hpp"
+#include "Anonymity/Tolerant/Accusation.hpp"
+#include "Anonymity/Tolerant/AlibiData.hpp"
+#include "Anonymity/Tolerant/BlameMatrix.hpp"
+#include "Anonymity/Tolerant/Conflict.hpp"
+#include "Anonymity/Tolerant/MessageHistory.hpp"
+#include "Anonymity/Tolerant/TolerantBulkRound.hpp"
 
 #include "Applications/CommandLine.hpp"
 #include "Applications/ConsoleSink.hpp"
@@ -110,6 +117,7 @@
 #include "Web/Services/WebService.hpp"
 
 using namespace Dissent::Anonymity;
+using namespace Dissent::Anonymity::Tolerant;
 using namespace Dissent::Applications;
 using namespace Dissent::Crypto;
 using namespace Dissent::Connections;