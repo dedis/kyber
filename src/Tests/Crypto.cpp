70475a7a751e98b2025cc834c5218d13b10cd119
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-03-28 16:41:32 -0400

    [Tests] Added a sanity test for CryptoPP::GDSA and a initial verification of the Neff key generation scheme

--- src/Tests/Crypto.cpp
+++ src/Tests/Crypto.cpp
@@ -348,5 +348,105 @@ namespace Tests {
     QScopedPointer<Library> lib(new CppLibrary());
     ZeroKnowledgeTest(lib.data(), false);
   }
+
+  TEST(Crypto, CppDsaNeff)
+  {
+    int keys = 50;
+    int servers = 10;
+    QSharedPointer<CppDsaPrivateKey> base_key(new CppDsaPrivateKey());
+    Integer generator = base_key->GetGenerator();
+    Integer subgroup = base_key->GetSubgroup();
+    Integer modulus = base_key->GetModulus();
+
+    QVector<QSharedPointer<CppDsaPrivateKey> > private_keys;
+    QVector<Integer> public_elements;
+
+    for(int idx = 0; idx < keys; idx++) {
+      QSharedPointer<CppDsaPrivateKey> private_key(
+          new CppDsaPrivateKey(modulus, subgroup, generator));
+      EXPECT_EQ(modulus, private_key->GetModulus());
+      EXPECT_EQ(subgroup, private_key->GetSubgroup());
+      EXPECT_EQ(generator, private_key->GetGenerator());
+      private_keys.append(private_key);
+
+      public_elements.append(private_key->GetPublicElement());
+      EXPECT_NE(generator, private_key->GetPublicElement());
+      EXPECT_NE(modulus, private_key->GetPublicElement());
+
+      EXPECT_TRUE(private_key->VerifyKey(*private_key->GetPublicKey()));
+      EXPECT_TRUE(private_key->GetPublicKey()->VerifyKey(*private_key)); 
+    }
+
+    for(int idx = 0; idx < servers; idx++) {
+      QSharedPointer<CppDsaPrivateKey> private_key(new CppDsaPrivateKey());
+      Integer local_generator = private_key->GetPrivateExponent();
+      generator = generator.Pow(local_generator, modulus);
+
+      for(int jdx = 0; jdx < keys; jdx++) {
+        public_elements[jdx] = public_elements[jdx].Pow(local_generator, modulus);
+      }
+    }
+
+    QVector<QSharedPointer<CppDsaPublicKey> > public_keys;
+
+    for(int idx = 0; idx < keys; idx++) {
+      QSharedPointer<CppDsaPrivateKey> private_key(
+          new CppDsaPrivateKey(modulus, subgroup, generator,
+            private_keys[idx]->GetPrivateExponent()));
+      private_keys[idx] = private_key;
+      
+      public_keys.append(QSharedPointer<CppDsaPublicKey>(
+            new CppDsaPublicKey(modulus, subgroup, generator, public_elements[idx])));
+    }
+
+    QScopedPointer<Random> rng(CryptoFactory::GetInstance().
+        GetLibrary()->GetRandomNumberGenerator());
+    QByteArray data(1500, 0);
+    rng->GenerateBlock(data);
+
+    for(int idx = 0; idx < keys; idx++) {
+      EXPECT_TRUE(private_keys[idx]->VerifyKey(*public_keys[idx].data()));
+      EXPECT_TRUE(public_keys[idx]->Verify(data, private_keys[idx]->Sign(data)));
+    }
+  }
+
+  TEST(Crypto, CppDsaSanityCheck)
+  {
+    CryptoPP::GDSA<CryptoPP::SHA256>::PrivateKey key;
+    CryptoPP::AutoSeededX917RNG<CryptoPP::DES_EDE3> rng;
+    key.GenerateRandom(rng,
+        CryptoPP::MakeParameters
+        (CryptoPP::Name::ModulusSize(), 2048)
+        (CryptoPP::Name::SubgroupOrderSize(), 256)); 
+    for(int idx = 0; idx < 4; idx++)
+      EXPECT_TRUE(key.Validate(rng, idx));
+
+    CryptoPP::GDSA<CryptoPP::SHA256>::PrivateKey key0;
+    key0.GenerateRandom(rng,
+        CryptoPP::MakeParameters
+        (CryptoPP::Name::Modulus(), key.GetGroupParameters().GetModulus())
+        (CryptoPP::Name::SubgroupOrder(), key.GetGroupParameters().GetSubgroupOrder())
+        (CryptoPP::Name::SubgroupGenerator(), key.GetGroupParameters().GetGenerator()));
+    for(int idx = 0; idx < 4; idx++)
+      EXPECT_TRUE(key0.Validate(rng, idx));
+
+    CryptoPP::GDSA<CryptoPP::SHA256>::PrivateKey key1;
+    key1.Initialize(rng, key.GetGroupParameters().GetModulus(), key.GetGroupParameters().GetGenerator());
+    // These fail since we ignore the subgroup
+    for(int idx = 0; idx < 4; idx++)
+      EXPECT_FALSE(key1.Validate(rng, idx));
+
+    CryptoPP::GDSA<CryptoPP::SHA256>::PrivateKey key2;
+    key2.Initialize(key.GetGroupParameters().GetModulus(), key.GetGroupParameters().GetSubgroupOrder(),
+        key.GetGroupParameters().GetGenerator(), key.GetPrivateExponent());
+    for(int idx = 0; idx < 4; idx++)
+      EXPECT_TRUE(key2.Validate(rng, idx));
+
+    CryptoPP::GDSA<CryptoPP::SHA256>::PrivateKey key3;
+    key3.GenerateRandomWithKeySize(rng, 1024);
+    for(int idx = 0; idx < 4; idx++)
+      EXPECT_TRUE(key3.Validate(rng, idx));
+
+  }
 }
 }
