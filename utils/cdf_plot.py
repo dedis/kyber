3f2d66ca1390514ebd9dfdadeba268e8c788995d
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-04-29 17:23:09 -0400

    [utils] made cdf_plot print much nicer plots

--- utils/cdf_plot.py
+++ utils/cdf_plot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 from matplotlib import pyplot, rc
-import getopt, numpy, sys
+#from scipy.stats import cumfreq
+#from statsmodels.tools.tools import ECDF
+import getopt, math, numpy, sys
 
 optlist, args = getopt.getopt(sys.argv[1:], "", ["output=",
     "ylabel=", "xlabel="])
@@ -42,20 +44,64 @@ def cdf(data):
   x = []
   y = []
   last = 0
+  last_key = 0
   for key in binkeys:
+    x.append(last_key)
+    last_key = key
     x.append(key)
     last += bins[key]
     y.append(last / count)
+    y.append(last / count)
   return x, y
 
+"""
+def cdf0(data):
+  num_bins = math.sqrt(len(data))
+  cf = cumfreq(data, num_bins)
+  x = numpy.linspace(min(data), max(data), num_bins)
+  return x, (cf[0] * 1.0) / (1.0 * cf[0][-1])
+"""
+
+"""
+def cdf1(data):
+  num_bins = math.sqrt(len(data))
+  ecdf = ECDF(data)
+  x = numpy.linspace(min(data), max(data), num_bins)
+  y = ecdf(x)
+  return x, y
+"""
+
+intensity_addend = 1.0 / len(args)
+intensity = 0
+marker_count = 5
+plot = pyplot.semilogx
+
 for filename in args:
-  x = numpy.loadtxt(filename, unpack=True)
-  data = sorted(x)
+  data = numpy.loadtxt(filename, unpack=True)
   x, y = cdf(data)
-  pyplot.plot(x, y, '-' + markers.pop(), label = filename)
+
+  x0 = []
+  y0 = []
+  x0.append(x[1])
+  y0.append(y[1])
+
+  inc = len(x) / marker_count
+  current = 1 + inc
+  for idx in range(marker_count - 1):
+    x0.append(x[current])
+    y0.append(y[current])
+    current += inc
+
+  x0.append(x[-2])
+  y0.append(y[-2])
+
+  plot(x, y, "-", label = filename, **{"color" : str(intensity)})
+  plot(x0, y0, markers.pop(), **{"color" : str(intensity)})
+
+  intensity += intensity_addend
 
 pyplot.legend(loc = "lower right")
-pyplot.subplots_adjust(bottom = .08, right = .98, top = .98, left = .08)
+pyplot.subplots_adjust(bottom = .12, right = .98, top = .98, left = .08)
 pyplot.ylabel(ylabel)
 pyplot.xlabel(xlabel)
 pyplot.savefig(output)
