7e14414d5c9993ad63d7fb23d4733dd2120f4208
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2012-06-29 15:26:05 -0400

    [Messaging] Added a new error type due to incompatible sender

--- src/Messaging/Response.hpp
+++ src/Messaging/Response.hpp
@@ -16,6 +16,7 @@ namespace Messaging {
     public:
       enum ErrorTypes {
         CorruptResponse,
+        InvalidSender,
         InvalidInput,
         InvalidMessage,
         InvalidMethod,
