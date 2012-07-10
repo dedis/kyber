23e618658f0f2607b2ddfbdaa23644e9d35644f7
apuljain <18apul@gmail.com>
2012-07-10 01:54:35 -0400

    [Tests] Null Auth code (minor modifications by David)

---
+++ src/Tests/AuthenticateTest.cpp
@@ -0,0 +1,31 @@
+#include "DissentTest.hpp"
+
+namespace Dissent {
+namespace Tests {
+
+  void AuthPass(const Id &authe_id, NullAuthenticate *authe, NullAuthenticator *autho)
+  {
+    QVariant m1 = authe->PrepareForChallenge();
+    QVariant m2 = autho->RequestChallenge(authe_id, m1);
+    QPair<bool,QVariant> r1 = authe->ProcessChallenge(m2);
+
+    EXPECT_TRUE(r1.first);
+
+    QPair<bool, PublicIdentity> r2 = autho->VerifyResponse(authe_id, r1.second);
+    EXPECT_TRUE(r2.first);
+    EXPECT_EQ(r2.second, GetPublicIdentity(authe->GetPrivateIdentity()));
+  }
+
+  TEST(NullAuthenticate, Base)
+  {
+    Crypto::Library *lib = Crypto::CryptoFactory::GetInstance().GetLibrary();
+    PrivateIdentity client(Id(),
+        QSharedPointer<AsymmetricKey>(lib->CreatePrivateKey()),
+        QSharedPointer<DiffieHellman>(lib->CreateDiffieHellman()));
+
+    NullAuthenticate authe(client);
+    NullAuthenticator autho;
+    AuthPass(client.GetLocalId(), &authe, &autho);
+  }
+}
+}
