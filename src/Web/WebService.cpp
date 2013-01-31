6f3b248bc19d25ccea462256b5cf484986c651d4
David Isaac Wolinsky <isaac.wolinsky@gmail.com>
2013-01-31 15:45:06 -0500

    [Web] By changing to the original library, we need to respect content-length in order to properly end transmissions ... likewise the library should probably close a connection if we haven't set keep-alive in the headers *shrug*

--- src/Web/WebService.cpp
+++ src/Web/WebService.cpp
@@ -17,13 +17,16 @@ namespace Web {
 
   void WebService::SendNotFound(QHttpResponse *response)
   {
+    QByteArray msg = "Error: Not Found";
+    response->setHeader("content-length", QString::number(msg.size()));
     response->writeHead(QHttpResponse::STATUS_NOT_FOUND);
-    response->write(QString("Error: Not Found"));
+    response->write(msg);
     response->end();
   }
 
   void WebService::SendResponse(QHttpResponse *response, const QByteArray &data)
   {
+    response->setHeader("content-length", QString::number(data.size()));
     response->writeHead(QHttpResponse::STATUS_OK);
     response->write(data);
     response->end();
@@ -34,8 +37,10 @@ namespace Web {
     bool success;
     QByteArray output = QtJson::Json::serialize(data, success);
     if(!success) {
+      response->setHeader("content-length", QString::number(0));
       response->writeHead(QHttpResponse::STATUS_INTERNAL_SERVER_ERROR);
     } else {
+      response->setHeader("content-length", QString::number(output.size()));
       response->writeHead(QHttpResponse::STATUS_OK);
       response->write(output);
     }
