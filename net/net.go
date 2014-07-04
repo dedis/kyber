20159d2e51b80c42f975d40fea173a25ef61c12a
Bryan Ford <brynosaurus@gmail.com>
2014-07-04 15:27:41 +0200

    First-cut crack at an "abstract network view" interface, with somewhat cleaned-up and generified SOCKS server code.

---
+++ net/net.go
@@ -0,0 +1,7 @@
+/*
+Package net defines an abstract interface for a network access viewpoint,
+and implementations of this interface for both
+the system's "native network viewpoint
+and useful virtual networking viewpoints.
+*/
+package net
