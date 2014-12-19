61b963f51a45bf42a14fdb7a75333b93bf7f3fef
Danny Jackowitz <daniel.jackowitz@yale.edu>
2014-12-19 15:50:00 -0500

    Update protobuf test case to use Constructors - resolves #2

--- protobuf/encoding_test.go
+++ protobuf/encoding_test.go
@@ -1,6 +1,7 @@
 package protobuf
 
 import (
+	"reflect"
 	"testing"
 
 	"github.com/dedis/crypto/abstract"
@@ -18,15 +19,25 @@ func (t *TestEncoding) Equal(t2 *TestEncoding) bool {
 	return t.S.Equal(t2.S) && t.P.Equal(t2.P)
 }
 
+var aSecret abstract.Secret
+var tSecret = reflect.TypeOf(&aSecret).Elem()
+
+var aPoint abstract.Point
+var tPoint = reflect.TypeOf(&aPoint).Elem()
+
 func testEncoding(t *testing.T, suite abstract.Suite) {
+	cons := Constructors {
+		tSecret: func()interface{} { return suite.Secret() },
+		tPoint: func()interface{} { return suite.Point() },
+	}
 	s := suite.Secret().One()
-	p := suite.Point().Mul(nil,s)
+	p := suite.Point().Mul(nil, s)
 	
 	test := TestEncoding{s, p}
 	buf := Encode(&test)
 
-	test2 := TestEncoding{suite.Secret(), suite.Point()}
-	err := Decode(buf, &test2, nil)
+	test2 := TestEncoding{}
+	err := Decode(buf, &test2, cons)
 	if err != nil || !test.Equal(&test2) {
 		t.Error(err)
 	}
