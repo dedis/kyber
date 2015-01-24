933c97605021fc0db60f0ede2e409d5048e160ce
webiii <webiii@localhost>
2015-01-24 12:33:30 -0500

    Initial commit for the testing suite.

---
+++ poly/sharing_test.go
@@ -0,0 +1,11 @@
+package poly
+
+import ()
+
+/* This file is a go testing suite for sharing.go. It provides
+ * multiple test cases for ensuring that encryption schemes built
+ * with this such as Shamir secret sharing are safe and secure.
+ *
+ * The tests can also server as a useful reference for how to work
+ * with the code.
+ */
