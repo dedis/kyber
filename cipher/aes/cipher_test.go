slifland37/crypto

Began writing a cipher test - Test for simple encrypt / decrypt - Encrypt / decrypt and authentication - Ensure sufficient randomness - Ensure that a single bit flip breaks authentication

cipher/aes/cipher_test.go
@@ -1,0 +1,10 @@
+package aes
+
+import (
+	"github.com/dedis/crypto/test"
+	"testing"
+)
+
+func TestAES(t *testing.T) {
+	test.BlockCipherTest(t, NewCipher128)
+}

dedis/kyber

cipher/aes/cipher_test.go
@@ -1,0 +1,10 @@
+package aes
+
+import (
+	"github.com/dedis/crypto/test"
+	"testing"
+)
+
+func TestAES(t *testing.T) {
+	test.CipherTest(t, NewCipher128)
+}