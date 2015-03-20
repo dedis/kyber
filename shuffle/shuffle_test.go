260f749cc9db9ffe33be4eb153f7c877d4df14b0
Bryan A Ford <brynosaurus@gmail.com>
2015-03-20 22:21:13 +0100

    Experimental: add simple binary-permutation (switch) shuffle based on Camenisch/Stadler proofs, for comparison and testing.

--- shuffle/shuffle_test.go
+++ shuffle/shuffle_test.go
@@ -7,8 +7,16 @@ import (
 	"testing"
 )
 
+func TestBiffle(t *testing.T) {
+	BiffleTest(edwards.NewAES128SHA256Ed25519(false), 1)
+}
+
 func TestPairShuffle(t *testing.T) {
 	TestShuffle(edwards.NewAES128SHA256Ed25519(false), 10, 1)
+}
+
+func BenchmarkBiffleP256(b *testing.B) {
+	BiffleTest(nist.NewAES128SHA256P256(), b.N)
 }
 
 func Benchmark2PairShuffleP256(b *testing.B) {
@@ -21,6 +29,10 @@ func Benchmark10PairShuffleP256(b *testing.B) {
 
 func Benchmark10PairShuffleOSSLP256(b *testing.B) {
 	TestShuffle(openssl.NewAES128SHA256P256(), 10, b.N)
+}
+
+func BenchmarkBiffleEd25519(b *testing.B) {
+	BiffleTest(edwards.NewAES128SHA256Ed25519(false), b.N)
 }
 
 func Benchmark2PairShuffleEd25519(b *testing.B) {
