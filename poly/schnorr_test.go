c489dd4305ea55ec02e41fb5b262b727f4d42929
Nicolas GAILLY <nikkolasg@gmail.com>
2015-09-16 12:00:35 +0200

    schnorr signature implemented + tested (again, need few more test to extend error part coverage)

---
+++ poly/schnorr_test.go
@@ -0,0 +1,181 @@
+package poly
+
+import (
+	"fmt"
+	"testing"
+)
+
+var msg []byte = []byte("Hello World!")
+
+func TestNewRound(t *testing.T) {
+	n := 3
+	pl := PolyInfo{edward, 2, n, n}
+	schnorrs := generateSchnorrStructs(pl)
+	randoms := generateSharedSecrets(pl)
+	randoms2 := generateSharedSecrets(pl)
+	for i, _ := range schnorrs {
+		err := schnorrs[i].NewRound(randoms[i], msg)
+		if err != nil {
+			t.Error(fmt.Sprintf("NewRound should validate : %v", err))
+		}
+		err = schnorrs[i].NewRound(randoms2[i], msg)
+		if err != nil {
+			t.Error(fmt.Sprintf("Second NewRound should validate : %v", err))
+		}
+		if schnorrs[i].random.Pub.Equal(randoms[i].Pub) {
+			t.Error(fmt.Sprintf("Random secret should not be equals"))
+		}
+
+	}
+}
+
+func TestRevealPartialSig(t *testing.T) {
+	n := 3
+	pl := PolyInfo{edward, 2, n, n}
+	schnorrs := generateSchnorrStructs(pl)
+	randoms := generateSharedSecrets(pl)
+	for i, _ := range schnorrs {
+		err := schnorrs[i].NewRound(randoms[i], msg)
+		if err != nil {
+			t.Error(fmt.Sprintf("NewRound should validate : %v", err))
+		}
+		ps := schnorrs[i].RevealPartialSig()
+		if ps.Index != schnorrs[i].index() {
+			t.Error(fmt.Sprintf("PartialSig does not have the same index as its producer ><"))
+		}
+		ps2 := schnorrs[i].RevealPartialSig()
+		if !(*ps.Part).Equal(*ps2.Part) {
+			t.Error(fmt.Sprintf("PartialSig does not return the same partial signature "))
+		}
+		err = schnorrs[i].verifyPartialSig(ps)
+		if err != nil {
+			t.Error(fmt.Sprintf("RevealPartialSig's sig could have not been verified : %v", err))
+		}
+	}
+}
+
+func TestAddPartialSig(t *testing.T) {
+	n := 3
+	pl := PolyInfo{edward, 2, n, n}
+	schnorrs := generateSchnorrStructs(pl)
+	randoms := generateSharedSecrets(pl)
+	for i, _ := range schnorrs {
+		err := schnorrs[i].NewRound(randoms[i], msg)
+		if err != nil {
+			t.Error(fmt.Sprintf("NewRound should validate : %v", err))
+		}
+	}
+	for i, _ := range schnorrs {
+		ps := schnorrs[i].RevealPartialSig()
+		if ps.Index != schnorrs[i].index() {
+			t.Error(fmt.Sprintf("PartialSig does not have the same index as its producer ><"))
+		}
+		// geive the partial sig to everyone
+		for j, _ := range schnorrs {
+			if err := schnorrs[j].AddPartialSig(ps); err != nil {
+				t.Error(fmt.Sprintf("AddPartialSig should validate (adding partial sig of peer %d to peer %d : %v", ps.Index, schnorrs[j].index(), err))
+			}
+		}
+
+	}
+
+	// test adding again sig
+	ps2 := schnorrs[0].RevealPartialSig()
+	err := schnorrs[0].AddPartialSig(ps2)
+	if err == nil {
+		t.Error(fmt.Sprintf("AddPartialSig 2 times should return an error"))
+	}
+	// nillify an partial sig then add a wrong one to same index
+	schnorrs[0].partials[ps2.Index] = nil
+	s := edward.Secret().One()
+	ps2.Part = &s
+	err = schnorrs[0].AddPartialSig(ps2)
+	if err == nil {
+		t.Error(fmt.Sprintf("AddPartialSig with wrong sig should return an error"))
+	}
+}
+
+func TestSchnorrSig(t *testing.T) {
+	n := 3
+	pl := PolyInfo{edward, 2, n, n}
+	schnorrs := generateSchnorrStructs(pl)
+	randoms := generateSharedSecrets(pl)
+	for i, _ := range schnorrs {
+		err := schnorrs[i].NewRound(randoms[i], msg)
+		if err != nil {
+			t.Error(fmt.Sprintf("NewRound should validate : %v", err))
+		}
+	}
+	for i, _ := range schnorrs {
+		ps := schnorrs[i].RevealPartialSig()
+		if ps.Index != schnorrs[i].index() {
+			t.Error(fmt.Sprintf("PartialSig does not have the same index as its producer ><"))
+		}
+		// geive the partial sig to everyone
+		for j, _ := range schnorrs {
+			if err := schnorrs[j].AddPartialSig(ps); err != nil {
+				t.Error(fmt.Sprintf("AddPartialSig should validate (adding partial sig of peer %d to peer %d : %v", ps.Index, schnorrs[j].index(), err))
+			}
+		}
+	}
+	sig := make([]*SchnorrSig, n)
+	for i, _ := range schnorrs {
+		s, err := schnorrs[i].SchnorrSig()
+		if err != nil {
+			t.Error(fmt.Sprintf("SchnorrSig should validate : %v", err))
+		}
+		sig[i] = s
+	}
+	// test equality of the signature amongst the peers
+	for i, _ := range sig {
+		for j, _ := range sig[i+1:] {
+			if !(*sig[i].signature).Equal(*sig[j].signature) {
+				t.Error(fmt.Sprintf("SchnorrSig should produce the same signature amongst peer (%d vs %d)", i, j))
+			}
+			if !(sig[i].random.Equal(sig[j].random)) {
+				t.Error(fmt.Sprintf("SchnorrSig should produce the same signature (random poly %d != %d", i, j))
+			}
+		}
+	}
+}
+
+func TestVerifySchnorrSig(t *testing.T) {
+	n := 3
+	pl := PolyInfo{edward, 2, n, n}
+	schnorrs := generateSchnorrStructs(pl)
+	randoms := generateSharedSecrets(pl)
+	for i, _ := range schnorrs {
+		err := schnorrs[i].NewRound(randoms[i], msg)
+		if err != nil {
+			t.Error(fmt.Sprintf("NewRound should validate : %v", err))
+		}
+	}
+	for i, _ := range schnorrs {
+		ps := schnorrs[i].RevealPartialSig()
+		if ps.Index != schnorrs[i].index() {
+			t.Error(fmt.Sprintf("PartialSig does not have the same index as its producer ><"))
+		}
+		// geive the partial sig to everyone
+		for j, _ := range schnorrs {
+			if err := schnorrs[j].AddPartialSig(ps); err != nil {
+				t.Error(fmt.Sprintf("AddPartialSig should validate (adding partial sig of peer %d to peer %d : %v", ps.Index, schnorrs[j].index(), err))
+			}
+		}
+	}
+	sig := make([]*SchnorrSig, n)
+	for i, _ := range schnorrs {
+		s, err := schnorrs[i].SchnorrSig()
+		if err != nil {
+			t.Error(fmt.Sprintf("SchnorrSig should validate : %v", err))
+		}
+		sig[i] = s
+	}
+
+	// Verify the signature amongst each peers
+	for i, _ := range schnorrs {
+		err := schnorrs[i].VerifySchnorrSig(sig[0], msg)
+		if err != nil {
+			t.Error(fmt.Sprintf("VerifySchnorrSig on peer %d should validate the signature : %v", i, err))
+		}
+	}
+}
