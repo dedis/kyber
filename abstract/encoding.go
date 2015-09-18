f94ef2ef80fdf2e866802534a107f12b56eb659d
Nicolas GAILLY <nikkolasg@gmail.com>
2015-09-18 15:31:05 +0200

    added int conversion to int64 encoding

--- abstract/encoding.go
+++ abstract/encoding.go
@@ -177,6 +177,11 @@ func (de *decoder) value(v reflect.Value, depth int) error {
 				return err
 			}
 		}
+	case reflect.Int:
+		var i int64
+		err := binary.Read(de.r, binary.BigEndian, &i)
+		v.SetInt(i)
+		return err
 	default:
 
 		return binary.Read(de.r, binary.BigEndian, v.Addr().Interface())
@@ -234,7 +239,9 @@ func (en *encoder) value(obj interface{}, depth int) error {
 				return err
 			}
 		}
-
+	case reflect.Int:
+		t := reflect.TypeOf(int64(0))
+		return binary.Write(en.w, binary.BigEndian, v.Convert(t).Interface())
 	default:
 		// Fall back to big-endian binary encoding
 		return binary.Write(en.w, binary.BigEndian, obj)
