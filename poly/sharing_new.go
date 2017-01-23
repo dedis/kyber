7803c65cbbbbb25eb627597a965ec9779968b283
Philipp Jovanovic <philipp@jovanovic.io>
2017-01-23 12:29:29 +0100

    further simplifications to sharing.go

--- poly/sharing_new.go
+++ poly/sharing_new.go
@@ -4,26 +4,18 @@ import "github.com/dedis/crypto/abstract"
 
 // PriShare represents an individual private share v = p(i).
 type PriShare struct {
-	i int             // Index of the private share
-	v abstract.Scalar // Value of the private share
-}
-
-// XXX: PriShare should probably have public parameters i and v
-
-// Info returns the index and the value of the private share
-func (p *PriShare) Info() (int, abstract.Scalar) {
-	return p.i, p.v
+	I int             // Index of the private share
+	V abstract.Scalar // Value of the private share
 }
 
 // PriPoly represents a secret sharing polynomial.
 type PriPoly struct {
 	g      abstract.Group    // Cryptographic group
-	t      int               // Secret sharing threshold // XXX: corresponds to len(coeffs)
 	coeffs []abstract.Scalar // Coefficients of the polynomial
 }
 
-// NewPriPoly creates a new secret sharing polynomial, with g the cryptographic
-// group, t the secret sharing threshold, and s the secret to be shared.
+// NewPriPoly creates a new secret sharing polynomial for the cryptographic
+// group g, the secret sharing threshold t, and the secret to be shared s.
 func NewPriPoly(g abstract.Group, t int, s abstract.Scalar) *PriPoly {
 	// XXX: old PriPoly.Pick()
 	return nil
@@ -63,6 +55,11 @@ func (p *PriPoly) Equal(q *PriPoly) bool {
 	return true
 }
 
+// Commit creates a public commitment polynomial for the given base point b.
+func (p *PriPoly) Commit(b abstract.Point) *PubPoly {
+	return nil
+}
+
 // XXX: Do we need that?
 func (p *PriPoly) String() String {
 	return nil
@@ -70,46 +67,38 @@ func (p *PriPoly) String() String {
 
 // xCoords creates an array of x-coordinates for Lagrange interpolation. In the
 // returned array, exactly t x-coordinates are non-nil.
-func (p *PriPoly) xCoords() []abstract.Scalar {
+func priXCoords() []abstract.Scalar {
+	// XXX: check if it's possible to merge with pubXCoords
 	return nil
 }
 
-// RecoverSecret reconstructs the shared secret using Lagrange interpolation.
-func (p *PriPoly) RecoverSecret(shares []PriShare, t int) abstract.Scalar {
+// RecoverSecret reconstructs the shared secret p(0) using Lagrange interpolation.
+func RecoverSecret(shares []PriShare, t int) abstract.Scalar {
 	// XXX: this is the old PriShares.Secret()
-	// XXX: maybe it should be even completely independent of the PriPoly struct
 	return nil
 }
 
 // PubShare represents an individual public share v = p(i).
 type PubShare struct {
-	i int            // Index of the public share
-	v abstract.Point // Value of the public share
-}
-
-// XXX: PubShare should probably have public parameters i and v
-
-// Info returns the index and the value of the public share
-func (p *PubShare) Info() (int, abstract.Point) {
-	return p.i, p.v
+	I int            // Index of the public share
+	V abstract.Point // Value of the public share
 }
 
 // PubPoly represents a public commitment polynomial to a secret sharing polynomial.
 type PubPoly struct {
 	g       abstract.Group   // Cryptographic group
 	b       abstract.Point   // Base point, nil for standard base
-	t       int              // Secret sharing threshold // XXX: corresponds to len(commits)
 	commits []abstract.Point // Commitments to polynomial coefficients
 }
 
-// NewPubPoly creates a new public commitment polynomial.
-func NewPubPoly(g abstract.Group, b abstract.Point, t int, n int) *PubPoly {
+// NewPubPoly creates a new public commitment polynomial
+func NewPubPoly(g abstract.Group, b abstract.Point, commits []abstract.Point) *PubPoly {
 	return nil
 }
 
-// Commit to a given private polynomial.
-func (p *PubPoly) Commit(q *PriPoly) {
-	// XXX: compute and fill the commits inplace
+// Info returns the base point and the commitments to the polynomial coefficients.
+func (p *PubPoly) Info() (abstract.Point, []abstract.Point) {
+	return nil, nil
 }
 
 // Threshold returns the secret sharing threshold.
@@ -118,7 +107,7 @@ func (p *PubPoly) Threshold() int {
 	return len(p.commits)
 }
 
-// SecretCommit returns the secret p(0), i.e., the constant term of the polynomial.
+// SecretCommit returns the secret commitment p(0), i.e., the constant term of the polynomial.
 func (p *PubPoly) SecretCommit() abstract.Point {
 	return nil
 }
@@ -158,14 +147,15 @@ func (p *PubPoly) String() string {
 
 // xCoords creates an array of x-coordinates for Lagrange interpolation. In the
 // returned array, exactly t x-coordinates are non-nil.
-func (p *PubPoly) xCoords() []abstract.Scalar {
+func pubXCoords() []abstract.Scalar {
+	// XXX: check if it's possible to merge with priXCoords
 	return nil
 }
 
-// RecoverCommit reconstructs the secret commitment using Lagrange interpolation.
-func (p *PubPoly) RecoverCommit(shares []PubShare, t int) abstract.Point {
+// RecoverCommit reconstructs the secret commitment p(0) using Lagrange interpolation.
+func RecoverCommit(shares []PubShare, t int) abstract.Point {
 	// XXX: this is the old PubShares.SecretCommit()
-	// XXX: maybe it should be even completely independent of the PubPoly struct
+	// XXX: uses pubXCoords
 	return nil
 }
 
