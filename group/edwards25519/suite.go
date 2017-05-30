9cffa8b5e3167b41b39b75bca641de0d30abd28b
nikkolasg <nicolas.gailly@epfl.ch>
2017-05-30 14:34:20 +0200

    exported group/<suites>

--- group/edwards25519/suite.go
+++ group/edwards25519/suite.go
@@ -10,34 +10,34 @@ import (
 	"gopkg.in/dedis/kyber.v1/cipher/sha3"
 )
 
-type suiteEd25519 struct {
+type SuiteEd25519 struct {
 	Curve
 }
 
 // SHA256 hash function
-func (s *suiteEd25519) Hash() hash.Hash {
+func (s *SuiteEd25519) Hash() hash.Hash {
 	return sha256.New()
 }
 
 // SHA3/SHAKE128 Sponge Cipher
-func (s *suiteEd25519) Cipher(key []byte, options ...interface{}) kyber.Cipher {
+func (s *SuiteEd25519) Cipher(key []byte, options ...interface{}) kyber.Cipher {
 	return sha3.NewShakeCipher128(key, options...)
 }
 
-func (s *suiteEd25519) Read(r io.Reader, objs ...interface{}) error {
+func (s *SuiteEd25519) Read(r io.Reader, objs ...interface{}) error {
 	return kyber.SuiteRead(s, r, objs)
 }
 
-func (s *suiteEd25519) Write(w io.Writer, objs ...interface{}) error {
+func (s *SuiteEd25519) Write(w io.Writer, objs ...interface{}) error {
 	return kyber.SuiteWrite(s, w, objs)
 }
 
-func (s *suiteEd25519) New(t reflect.Type) interface{} {
+func (s *SuiteEd25519) New(t reflect.Type) interface{} {
 	return kyber.SuiteNew(s, t)
 }
 
 // Ciphersuite based on AES-128, SHA-256, and the Ed25519 curve.
-func NewAES128SHA256Ed25519(fullGroup bool) *suiteEd25519 {
-	suite := new(suiteEd25519)
+func NewAES128SHA256Ed25519(fullGroup bool) *SuiteEd25519 {
+	suite := new(SuiteEd25519)
 	return suite
 }
--- group/nist/suite.go
+++ group/nist/suite.go
@@ -12,33 +12,33 @@ import (
 	"gopkg.in/dedis/kyber.v1/util/random"
 )
 
-type suite128 struct {
+type Suite128 struct {
 	p256
 }
 
 // SHA256 hash function
-func (s *suite128) Hash() hash.Hash {
+func (s *Suite128) Hash() hash.Hash {
 	return sha256.New()
 }
 
 // SHA3/SHAKE128 Sponge Cipher
-func (s *suite128) Cipher(key []byte, options ...interface{}) kyber.Cipher {
+func (s *Suite128) Cipher(key []byte, options ...interface{}) kyber.Cipher {
 	return sha3.NewShakeCipher128(key, options...)
 }
 
-func (s *suite128) Read(r io.Reader, objs ...interface{}) error {
+func (s *Suite128) Read(r io.Reader, objs ...interface{}) error {
 	return kyber.SuiteRead(s, r, objs)
 }
 
-func (s *suite128) Write(w io.Writer, objs ...interface{}) error {
+func (s *Suite128) Write(w io.Writer, objs ...interface{}) error {
 	return kyber.SuiteWrite(s, w, objs)
 }
 
-func (s *suite128) New(t reflect.Type) interface{} {
+func (s *Suite128) New(t reflect.Type) interface{} {
 	return kyber.SuiteNew(s, t)
 }
 
-func (s *suite128) NewKey(rand cipher.Stream) kyber.Scalar {
+func (s *Suite128) NewKey(rand cipher.Stream) kyber.Scalar {
 	if rand == nil {
 		rand = random.Stream
 	}
@@ -46,8 +46,8 @@ func (s *suite128) NewKey(rand cipher.Stream) kyber.Scalar {
 }
 
 // Ciphersuite based on AES-128, SHA-256, and the NIST P-256 elliptic curve.
-func NewAES128SHA256P256() *suite128 {
-	suite := new(suite128)
+func NewAES128SHA256P256() *Suite128 {
+	suite := new(Suite128)
 	suite.p256.Init()
 	return suite
 }
