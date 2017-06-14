28b9eb96cc70a7f97d846006e4c5934159d480e5
nikkolasg <nicolas.gailly@epfl.ch>
2017-06-14 09:38:41 +0200

    moved pbc/ wrapper into v1_dfinity branch to make travis happy

--- group/pbc/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-
-dfinityPath="$HOME/prog/dfinity/crypto"
-dedisPath="$GOPATH/src/gopkg.in"
-docker rm -f dfinity
-docker run --rm -it --name dfinity -v $dfinityPath:/workspace -v $dedisPath:/workspace/go/src/gopkg.in dfinity/build 
