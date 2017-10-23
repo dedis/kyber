1dce6ef518eac6ed4a95c0ee52221c46a983501b
Linus Gasser <linus.gasser@epfl.ch>
2017-10-23 16:54:03 +0200

    allowing doc to compile

--- group/nist/doc.go
+++ group/nist/doc.go
@@ -2,5 +2,4 @@
 // based on the NIST standards, using Go's built-in crypto library.
 // Since that package does not implement constant time arithmetic operations
 // yet, it must be compiled with the "vartime" compilation flag.
-
 package nist
