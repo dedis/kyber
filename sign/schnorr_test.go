cbc678c673ea3410f01ef573c443c6be92ae9209
Linus Gasser <linus.gasser@epfl.ch>
2017-11-01 14:14:09 +0100

    adding static example

--- sign/schnorr_test.go
+++ sign/schnorr_test.go
@@ -1,6 +1,7 @@
 package sign
 
 import (
+	"encoding/hex"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -60,5 +61,21 @@ func TestEdDSACompatibility(t *testing.T) {
 	if err != nil {
 		t.Fatalf("Couldn't verify signature: \n%+v\nfor msg:'%s'. Error:\n%v", s, msg, err)
 	}
+}
 
+func TestSchnorrSignature2(t *testing.T) {
+	suite := ed25519.NewAES128SHA256Ed25519(false)
+	msg := []byte("Hello Schnorr")
+	sigStr := "b95fc52a5fd2e18aa7ace5b2250c2a25e368f75c148ea3403c8f32b5f100781b" +
+		"362c668aab4cf50eafdc2fcf45214c0dfbe86fce72e4632158c02c571e977306"
+	sig, _ := hex.DecodeString(sigStr)
+	pubStr := "59d7fd947fc88e47d3f878e82e26629dea7a28e8d4233f11068a6b464e195bfd"
+	pubBuf, _ := hex.DecodeString(pubStr)
+	pub := suite.Point()
+	pub.UnmarshalBinary(pubBuf)
+
+	if err := VerifySchnorr(suite, pub, msg, sig); err != nil {
+		t.Fatalf("Wrong schnorr signature: %s", err)
+	}
+	t.Log("Successfully verified schnorr signature")
 }
