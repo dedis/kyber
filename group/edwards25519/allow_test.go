c95cd8e3e8dba190583f85056bb1458d4203838a
Jeff R. Allen <jeff.allen@epfl.ch>
2017-11-30 12:57:51 +0100

    Fix test.

--- group/edwards25519/allow_test.go
+++ group/edwards25519/allow_test.go
@@ -9,7 +9,7 @@ import (
 )
 
 func TestNotVartime(t *testing.T) {
-	p := testSuite.Point()
+	p := NewAES128SHA256Ed25519().Point()
 	if _, ok := p.(kyber.AllowsVarTime); ok {
 		t.Fatal("expected Point to NOT allow var time")
 	}
