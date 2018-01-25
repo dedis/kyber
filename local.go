86fde45e0f69c0f8dd60f0988e2ec694c9782c12
Jeff R. Allen <jeff.allen@epfl.ch>
2018-01-25 17:11:09 +0100

    Give a clear error if a LocalTest is used after CloseAll.

--- local.go
+++ local.go
@@ -38,6 +38,9 @@ type LocalTest struct {
 	ctx   *network.LocalManager
 	Suite network.Suite
 	path  string
+	// Once closed is set, do not allow further operation on it,
+	// since now the temp directory is gone.
+	closed bool
 }
 
 const (
@@ -80,6 +83,7 @@ func NewTCPTest(s network.Suite) *LocalTest {
 // StartProtocol takes a name and a tree and will create a
 // new Node with the protocol 'name' running from the tree-root
 func (l *LocalTest) StartProtocol(name string, t *Tree) (ProtocolInstance, error) {
+	l.panicClosed()
 	rootServerIdentityID := t.Root.ServerIdentity.ID
 	for _, h := range l.Servers {
 		if h.ServerIdentity.ID.Equal(rootServerIdentityID) {
@@ -94,6 +98,7 @@ func (l *LocalTest) StartProtocol(name string, t *Tree) (ProtocolInstance, error
 // CreateProtocol takes a name and a tree and will create a
 // new Node with the protocol 'name' without running it
 func (l *LocalTest) CreateProtocol(name string, t *Tree) (ProtocolInstance, error) {
+	l.panicClosed()
 	rootServerIdentityID := t.Root.ServerIdentity.ID
 	for _, h := range l.Servers {
 		if h.ServerIdentity.ID.Equal(rootServerIdentityID) {
@@ -107,6 +112,7 @@ func (l *LocalTest) CreateProtocol(name string, t *Tree) (ProtocolInstance, erro
 
 // GenServers returns n Servers with a localRouter
 func (l *LocalTest) GenServers(n int) []*Server {
+	l.panicClosed()
 	servers := l.genLocalHosts(n)
 	for _, server := range servers {
 		l.Servers[server.ServerIdentity.ID] = server
@@ -120,6 +126,7 @@ func (l *LocalTest) GenServers(n int) []*Server {
 // GenTree will create a tree of n servers with a localRouter, and returns the
 // list of servers and the associated roster / tree.
 func (l *LocalTest) GenTree(n int, register bool) ([]*Server, *Roster, *Tree) {
+	l.panicClosed()
 	servers := l.GenServers(n)
 
 	list := l.GenRosterFromHost(servers...)
@@ -140,6 +147,7 @@ func (l *LocalTest) GenTree(n int, register bool) ([]*Server, *Roster, *Tree) {
 // nbrServers can be smaller than nbrTreeNodes, in which case a given server will
 // be used more than once in the tree.
 func (l *LocalTest) GenBigTree(nbrTreeNodes, nbrServers, bf int, register bool) ([]*Server, *Roster, *Tree) {
+	l.panicClosed()
 	servers := l.GenServers(nbrServers)
 
 	list := l.GenRosterFromHost(servers...)
@@ -155,6 +163,7 @@ func (l *LocalTest) GenBigTree(nbrTreeNodes, nbrServers, bf int, register bool) 
 // GenRosterFromHost takes a number of servers as arguments and creates
 // an Roster.
 func (l *LocalTest) GenRosterFromHost(servers ...*Server) *Roster {
+	l.panicClosed()
 	var entities []*network.ServerIdentity
 	for i := range servers {
 		entities = append(entities, servers[i].ServerIdentity)
@@ -162,6 +171,12 @@ func (l *LocalTest) GenRosterFromHost(servers ...*Server) *Roster {
 	list := NewRoster(entities)
 	l.Rosters[list.ID] = list
 	return list
+}
+
+func (l *LocalTest) panicClosed() {
+	if l.closed {
+		panic("attempt to use LocalTest after CloseAll")
+	}
 }
 
 // CloseAll takes a list of servers that will be closed
@@ -187,10 +202,12 @@ func (l *LocalTest) CloseAll() {
 	l.ctx.Stop()
 	l.Nodes = make([]*TreeNodeInstance, 0)
 	os.RemoveAll(l.path)
+	l.closed = true
 }
 
 // getTree returns the tree of the given TreeNode
 func (l *LocalTest) getTree(tn *TreeNode) *Tree {
+	l.panicClosed()
 	var tree *Tree
 	for _, t := range l.Trees {
 		if tn.IsInTree(t) {
@@ -203,6 +220,7 @@ func (l *LocalTest) getTree(tn *TreeNode) *Tree {
 
 // NewTreeNodeInstance creates a new node on a TreeNode
 func (l *LocalTest) NewTreeNodeInstance(tn *TreeNode, protName string) (*TreeNodeInstance, error) {
+	l.panicClosed()
 	o := l.Overlays[tn.ServerIdentity.ID]
 	if o == nil {
 		return nil, errors.New("Didn't find corresponding overlay")
@@ -230,6 +248,7 @@ func (l *LocalTest) NewTreeNodeInstance(tn *TreeNode, protName string) (*TreeNod
 
 // getNodes returns all Nodes that belong to a treeNode
 func (l *LocalTest) getNodes(tn *TreeNode) []*TreeNodeInstance {
+	l.panicClosed()
 	var nodes []*TreeNodeInstance
 	for _, n := range l.Overlays[tn.ServerIdentity.ID].instances {
 		nodes = append(nodes, n)
@@ -240,6 +259,7 @@ func (l *LocalTest) getNodes(tn *TreeNode) []*TreeNodeInstance {
 // sendTreeNode injects a message directly in the Overlay-layer, bypassing
 // Host and Network
 func (l *LocalTest) sendTreeNode(proto string, from, to *TreeNodeInstance, msg network.Message) error {
+	l.panicClosed()
 	if !from.Tree().ID.Equal(to.Tree().ID) {
 		return errors.New("Can't send from one tree to another")
 	}
@@ -257,12 +277,14 @@ func (l *LocalTest) sendTreeNode(proto string, from, to *TreeNodeInstance, msg n
 // known trees, also triggering dispatching of onet-messages waiting for that
 // tree
 func (l *LocalTest) addPendingTreeMarshal(c *Server, tm *TreeMarshal) {
+	l.panicClosed()
 	c.overlay.addPendingTreeMarshal(tm)
 }
 
 // checkPendingTreeMarshal looks whether there are any treeMarshals to be
 // called
 func (l *LocalTest) checkPendingTreeMarshal(c *Server, el *Roster) {
+	l.panicClosed()
 	c.overlay.checkPendingTreeMarshal(el)
 }
 
@@ -285,6 +307,7 @@ func (l *LocalTest) GetServices(servers []*Server, sid ServiceID) []Service {
 // returns the Service object of the first server in the list having sid as a
 // ServiceID.
 func (l *LocalTest) MakeHELS(nbr int, sid ServiceID, s network.Suite) ([]*Server, *Roster, Service) {
+	l.panicClosed()
 	servers := l.GenServers(nbr)
 	el := l.GenRosterFromHost(servers...)
 	return servers, el, l.Services[servers[0].ServerIdentity.ID][sid]
@@ -375,6 +398,7 @@ func (l *LocalTest) NewClient(serviceName string) *Client {
 
 // genLocalHosts returns n servers created with a localRouter
 func (l *LocalTest) genLocalHosts(n int) []*Server {
+	l.panicClosed()
 	servers := make([]*Server, n)
 	for i := 0; i < n; i++ {
 		port := 2000 + i*10
@@ -387,6 +411,7 @@ func (l *LocalTest) genLocalHosts(n int) []*Server {
 // TCP or Local. If it's TCP, then an available port is used, otherwise, the
 // port given in argument is used.
 func (l *LocalTest) NewServer(port int, s network.Suite) *Server {
+	l.panicClosed()
 	var server *Server
 	switch l.mode {
 	case TCP:
@@ -399,6 +424,7 @@ func (l *LocalTest) NewServer(port int, s network.Suite) *Server {
 
 // NewTCPServer returns a new TCP Server attached to this LocalTest.
 func (l *LocalTest) newTCPServer(s network.Suite) *Server {
+	l.panicClosed()
 	server := newTCPServer(0, s, l.path)
 	l.Servers[server.ServerIdentity.ID] = server
 	l.Overlays[server.ServerIdentity.ID] = server.overlay
@@ -410,6 +436,7 @@ func (l *LocalTest) newTCPServer(s network.Suite) *Server {
 // NewLocalServer returns a fresh Host using local connections within the context
 // of this LocalTest
 func (l *LocalTest) NewLocalServer(port int, s network.Suite) *Server {
+	l.panicClosed()
 	priv, id := NewPrivIdentity(s, port)
 	localRouter, err := network.NewLocalRouterWithManager(l.ctx, id, s)
 	if err != nil {
