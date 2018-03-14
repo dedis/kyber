881e2f327f7e7dec9a6d1b79b01918204e526a59
Philipp Jovanovic <philipp@jovanovic.io>
2018-03-14 11:52:57 +0100

    remove secret sharing workaround from pairing code

--- sign/tbls/tbls_test.go
+++ sign/tbls/tbls_test.go
@@ -13,12 +13,11 @@ func TestTBLS(test *testing.T) {
 	var err error
 	msg := []byte("Hello threshold Boneh-Lynn-Shacham")
 	suite := bn256.NewSuite()
-	g2s := bn256.NewSingleGroupSuite(suite.G2())
 	n := 10
 	t := n/2 + 1
 	secret := suite.G1().Scalar().Pick(suite.RandomStream())
-	priPoly := share.NewPriPoly(g2s, t, secret, suite.RandomStream())
-	pubPoly := priPoly.Commit(g2s.Point().Base())
+	priPoly := share.NewPriPoly(suite.G2(), t, secret, suite.RandomStream())
+	pubPoly := priPoly.Commit(suite.G2().Point().Base())
 	sigShares := make([][]byte, 0)
 	for _, x := range priPoly.Shares(n) {
 		sig, err := Sign(suite, x, msg)
