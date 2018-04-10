7949227e31db3743e55a9862d409fe9fd3fa8df9
nicolas gailly <nicolas.gailly@epfl.ch>
2018-04-10 12:42:43 +0200

    put back in constant time

--- suites/all_vartime.go
+++ suites/all_vartime.go
@@ -5,7 +5,6 @@ package suites
 import (
 	"github.com/dedis/kyber/group/curve25519"
 	"github.com/dedis/kyber/group/nist"
-	"github.com/dedis/kyber/pairing/bn256"
 )
 
 func init() {
@@ -13,8 +12,4 @@ func init() {
 	register(curve25519.NewBlakeSHA256Curve25519(true))
 	register(nist.NewBlakeSHA256P256())
 	register(nist.NewBlakeSHA256QR512())
-	register(bn256.NewSuite().G1().(Suite))
-	register(bn256.NewSuite().G2().(Suite))
-	register(bn256.NewSuite().GT().(Suite))
-
 }
