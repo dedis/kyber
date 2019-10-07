96e27debc0e707fef6451a16097c142f85b4a13d
PizzaWhisperer <mathilde.raynal@epfl.ch>
2019-10-07 18:50:27 +0200

    tests again

--- util/random/rand_test.go
+++ util/random/rand_test.go
@@ -57,17 +57,30 @@ func TestCryptoOnly(t *testing.T) {
 }
 
 func TestUserOnly(t *testing.T) {
-	r1 := strings.NewReader("some io.Reader stream to be used for testing")
-	cipher1 := NewMixedStream(r1)
+	seed := "some io.Reader stream to be used for testing"
+	cipher1 := NewMixedStream(strings.NewReader(seed))
 	src := make([]byte, size)
 	copy(src, []byte("hello"))
 	dst1 := make([]byte, size)
 	cipher1.XORKeyStream(dst1, src)
-	r2 := strings.NewReader("some io.Reader stream to be used for testing")
-	cipher2 := NewMixedStream(r2)
+	cipher2 := NewMixedStream(strings.NewReader(seed))
 	dst2 := make([]byte, size)
 	cipher2.XORKeyStream(dst2, src)
 	if !bytes.Equal(dst1, dst2) {
 		t.Fatal("dst1/dst2 should be equal")
 	}
+}
+
+func TestIncorrectSize(t *testing.T) {
+	//expecting a panic
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fatal("code did not panicked but should have")
+		}
+	}()
+	cipher := NewMixedStream()
+	src := make([]byte, size)
+	copy(src, []byte("hello"))
+	dst := make([]byte, size+1)
+	cipher.XORKeyStream(dst, src)
 }
