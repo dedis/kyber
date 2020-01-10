0770c5dd5b283c1f4528ac6b492dff242cd57b6b
nikkolasg <nikkolasg@gmail.com>
2020-01-10 22:32:23 +0000

    moved bn256 bls to bn256 package

--- sign/test/scheme.go
+++ sign/test/scheme.go
@@ -39,3 +39,57 @@ func SchemeTesting(t *testing.T, s sign.Scheme) {
 		}
 	})
 }
+
+// AggregationTesting test an aggrgatable scheme
+func AggregationTesting(t *testing.T, s sign.AggregatableScheme) {
+	t.Run("Aggregation valid", func(tt *testing.T) {
+		msg := []byte("Hello Boneh-Lynn-Shacham")
+		private1, public1 := s.NewKeyPair(random.New())
+		private2, public2 := s.NewKeyPair(random.New())
+		sig1, err := s.Sign(private1, msg)
+		require.Nil(tt, err)
+		sig2, err := s.Sign(private2, msg)
+		require.Nil(tt, err)
+		aggregatedSig, err := s.AggregateSignatures(sig1, sig2)
+		require.Nil(tt, err)
+		aggregatedKey := s.AggregatePublicKeys(public1, public2)
+
+		err = s.Verify(aggregatedKey, msg, aggregatedSig)
+		require.Nil(tt, err)
+	})
+	t.Run("Aggregation with invalid sig", func(tt *testing.T) {
+		msg := []byte("Hello Boneh-Lynn-Shacham")
+		private1, public1 := s.NewKeyPair(random.New())
+		private2, public2 := s.NewKeyPair(random.New())
+		sig1, err := s.Sign(private1, msg)
+		require.Nil(tt, err)
+		sig2, err := s.Sign(private2, msg)
+		require.Nil(tt, err)
+		aggregatedSig, err := s.AggregateSignatures(sig1, sig2)
+		require.Nil(tt, err)
+		aggregatedKey := s.AggregatePublicKeys(public1, public2)
+
+		aggregatedSig[0] ^= 0x01
+		if s.Verify(aggregatedKey, msg, aggregatedSig) == nil {
+			tt.Fatal("bls: verification succeeded unexpectedly")
+		}
+	})
+
+	t.Run("Aggregation with invalid public", func(tt *testing.T) {
+		msg := []byte("Hello Boneh-Lynn-Shacham")
+		private1, public1 := s.NewKeyPair(random.New())
+		private2, public2 := s.NewKeyPair(random.New())
+		_, public3 := s.NewKeyPair(random.New())
+		sig1, err := s.Sign(private1, msg)
+		require.Nil(tt, err)
+		sig2, err := s.Sign(private2, msg)
+		require.Nil(tt, err)
+		aggregatedSig, err := s.AggregateSignatures(sig1, sig2)
+		require.Nil(tt, err)
+		badAggregatedKey := s.AggregatePublicKeys(public1, public2, public3)
+
+		if s.Verify(badAggregatedKey, msg, aggregatedSig) == nil {
+			t.Fatal("bls: verification succeeded unexpectedly")
+		}
+	})
+}
