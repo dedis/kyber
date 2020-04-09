5c9abe52bd108deec7815386202114d522399917
nikkolasg <nikkolasg@gmail.com>
2020-04-09 20:09:04 +0100

    returning err only if not enough valid partials

--- sign/tbls/tbls.go
+++ sign/tbls/tbls.go
@@ -116,24 +116,27 @@ func (s *scheme) VerifyRecovered(public kyber.Point, msg, sig []byte) error {
 // shared public key X. The shared public key can be computed by evaluating the
 // public sharing polynomial at index 0.
 func (s *scheme) Recover(public *share.PubPoly, msg []byte, sigs [][]byte, t, n int) ([]byte, error) {
-	pubShares := make([]*share.PubShare, 0)
+	var pubShares []*share.PubShare
 	for _, sig := range sigs {
 		sh := SigShare(sig)
 		i, err := sh.Index()
 		if err != nil {
-			return nil, err
+			continue
 		}
 		if err = s.Scheme.Verify(public.Eval(i).V, msg, sh.Value()); err != nil {
-			return nil, err
+			continue
 		}
 		point := s.sigGroup.Point()
 		if err := point.UnmarshalBinary(sh.Value()); err != nil {
-			return nil, err
+			continue
 		}
 		pubShares = append(pubShares, &share.PubShare{I: i, V: point})
 		if len(pubShares) >= t {
 			break
 		}
+	}
+	if len(pubShares) < t {
+		return nil, errors.New("not enough valid partial signatures")
 	}
 	commit, err := share.RecoverCommit(s.sigGroup, pubShares, t, n)
 	if err != nil {
