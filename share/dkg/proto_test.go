133b380be2e5abfd0ae76358d4312ec65280ce29
nikkolasg <nikkolasg@gmail.com>
2020-04-19 20:40:43 +0100

    fast sync tested

--- share/dkg/proto_test.go
+++ share/dkg/proto_test.go
@@ -56,6 +56,7 @@ type TestBoard struct {
 	newResps chan AuthResponseBundle
 	newJusts chan AuthJustifBundle
 	network  *TestNetwork
+	badDeal  bool
 }
 
 func NewTestBoard(index uint32, n int, network *TestNetwork) *TestBoard {
@@ -69,6 +70,9 @@ func NewTestBoard(index uint32, n int, network *TestNetwork) *TestBoard {
 }
 
 func (t *TestBoard) PushDeals(d AuthDealBundle) {
+	if t.badDeal {
+		d.Bundle.Deals[0].EncryptedShare = []byte("bad bad bad")
+	}
 	t.network.BroadcastDeal(d)
 }
 
@@ -146,14 +150,17 @@ func TestProtoFull(t *testing.T) {
 	for _, node := range tns {
 		go node.phaser.Start()
 	}
-	// move three periods: start where node send deals, then nodes receive deal
-	// and send responses, then nodes processes response
-	// since there is no faults we expect to receive all results
-	moveTime(tns, period)
 	time.Sleep(100 * time.Millisecond)
-	moveTime(tns, period)
-	time.Sleep(100 * time.Millisecond)
-	moveTime(tns, period)
+	// move two periods:
+	// nodes already sent they deals, so they need to receive them after one
+	// period, then they send their responses. Second period to receive the
+	// responses, and then they send the justifications, if any.
+	// since there is no faults we expect to receive the result only after two
+	// periods.
+	for i := 0; i < 2; i++ {
+		moveTime(tns, period)
+		time.Sleep(100 * time.Millisecond)
+	}
 
 	// expect all results
 	var results []*Result
@@ -202,14 +209,17 @@ func TestProtoResharing(t *testing.T) {
 	for _, node := range tns {
 		go node.phaser.Start()
 	}
-	// move three periods: start where node send deals, then nodes receive deal
-	// and send responses, then nodes processes response
-	// since there is no faults we expect to receive all results
-	moveTime(tns, period)
 	time.Sleep(100 * time.Millisecond)
-	moveTime(tns, period)
-	time.Sleep(100 * time.Millisecond)
-	moveTime(tns, period)
+	// move two periods:
+	// nodes already sent they deals, so they need to receive them after one
+	// period, then they send their responses. Second period to receive the
+	// responses, and then they send the justifications, if any.
+	// since there is no faults we expect to receive the result only after two
+	// periods.
+	for i := 0; i < 2; i++ {
+		moveTime(tns, period)
+		time.Sleep(100 * time.Millisecond)
+	}
 
 	// expect all results
 	var results []*Result
@@ -263,16 +273,16 @@ func TestProtoResharing(t *testing.T) {
 	for _, node := range newTns {
 		go node.phaser.Start()
 	}
-	// move three periods: start where node send deals, then nodes receive deal
-	// and send responses, then nodes processes response and send justifs.then
-	// last period to wait for justifs and return result
-	moveTime(newTns, period)
 	time.Sleep(100 * time.Millisecond)
-	moveTime(newTns, period)
-	time.Sleep(100 * time.Millisecond)
-	moveTime(newTns, period)
-	time.Sleep(100 * time.Millisecond)
-	moveTime(newTns, period)
+	// move three periods:
+	// nodes already sent they deals, so they need to receive them after one
+	// period, then they send their responses. Second period to receive the
+	// responses, and then they send the justifications, if any. A third period
+	// is needed to receive all justifications.
+	for i := 0; i < 3; i++ {
+		moveTime(newTns, period)
+		time.Sleep(100 * time.Millisecond)
+	}
 
 	// expect all results
 	results = nil
@@ -318,19 +328,16 @@ func TestProtoThreshold(t *testing.T) {
 	for _, node := range tns {
 		go node.phaser.Start()
 	}
-	// move three periods: start where node send deals, then nodes receive deal
-	// and send responses, then nodes processes response
-	// since there is no faults we expect to receive all results
-	moveTime(tns, period)
 	time.Sleep(100 * time.Millisecond)
-	moveTime(tns, period)
-	time.Sleep(100 * time.Millisecond)
-	moveTime(tns, period)
-	time.Sleep(100 * time.Millisecond)
-	moveTime(tns, period)
-	time.Sleep(100 * time.Millisecond)
-	moveTime(tns, period)
-
+	// move three periods:
+	// nodes already sent they deals, so they need to receive them after one
+	// period, then they send their responses. Second period to receive the
+	// responses, and then they send the justifications, if any. A third period
+	// is needed to receive all justifications.
+	for i := 0; i < 3; i++ {
+		moveTime(tns, period)
+		time.Sleep(100 * time.Millisecond)
+	}
 	// expect all results
 	var results []*Result
 	for optRes := range resCh {
@@ -342,4 +349,107 @@ func TestProtoThreshold(t *testing.T) {
 	}
 	testResults(t, suite, thr, n, results)
 
+}
+
+func TestProtoFullFast(t *testing.T) {
+	n := 5
+	thr := n
+	period := 1 * time.Second
+	suite := edwards25519.NewBlakeSHA256Ed25519()
+	tns := GenerateTestNodes(suite, n)
+	list := NodesFromTest(tns)
+	network := NewTestNetwork(n)
+	dkgConf := DkgConfig{
+		FastSync:  true,
+		Suite:     suite,
+		NewNodes:  list,
+		Threshold: thr,
+	}
+	protoConf := Config{
+		Auth: schnorr.NewScheme(suite),
+	}
+	SetupNodes(tns, &dkgConf)
+	SetupProto(tns, &dkgConf, &protoConf, period, network)
+
+	var resCh = make(chan OptionResult, 1)
+	// start all nodes and wait until each end
+	for _, node := range tns {
+		go node.proto.Start()
+		go func(n *TestNode) { resCh <- <-n.proto.WaitEnd() }(node)
+	}
+	// start the phasers
+	for _, node := range tns {
+		// every node will start when phase starts
+		go node.phaser.Start()
+	}
+
+	// expect all results
+	var results []*Result
+	for optRes := range resCh {
+		require.NoError(t, optRes.Error)
+		results = append(results, optRes.Result)
+		if len(results) == n {
+			break
+		}
+	}
+	testResults(t, suite, thr, n, results)
+}
+
+func TestProtoThresholdFast(t *testing.T) {
+	n := 5
+	thr := 4
+	period := 1 * time.Second
+	suite := edwards25519.NewBlakeSHA256Ed25519()
+	tns := GenerateTestNodes(suite, n)
+	list := NodesFromTest(tns)
+	network := NewTestNetwork(n)
+	dkgConf := DkgConfig{
+		FastSync:  true,
+		Suite:     suite,
+		NewNodes:  list,
+		Threshold: thr,
+	}
+	protoConf := Config{
+		Auth: schnorr.NewScheme(suite),
+	}
+	SetupNodes(tns, &dkgConf)
+	SetupProto(tns, &dkgConf, &protoConf, period, network)
+	// set a node that will send a bad deal such that all deals are received
+	// "fast", then the normal rounds are happening
+	network.BoardFor(1).badDeal = true
+
+	var resCh = make(chan OptionResult, 1)
+	// start all nodes and wait until each end
+	for _, node := range tns {
+		go node.proto.Start()
+		if node.Index != 1 {
+			go func(n *TestNode) { resCh <- <-n.proto.WaitEnd() }(node)
+		}
+	}
+	// start the phasers
+	for _, node := range tns {
+		go node.phaser.Start()
+	}
+	time.Sleep(100 * time.Millisecond)
+	// move three periods:
+	// nodes already sent they deals, so they need to receive them after one
+	// period, then they send their responses. Second period to receive the
+	// responses, and then they send the justifications, if any. A third period
+	// is needed to receive all justifications.
+	// NOTE the first period is ignored by the protocol but timer still sends
+	// it.
+	for i := 0; i < 3; i++ {
+		moveTime(tns, period)
+		time.Sleep(100 * time.Millisecond)
+	}
+	// expect all results consistent except for the node 1
+	var results []*Result
+	for optRes := range resCh {
+		require.NoError(t, optRes.Error)
+		results = append(results, optRes.Result)
+		if len(results) == n-1 {
+			break
+		}
+	}
+	testResults(t, suite, thr, n, results)
 }
