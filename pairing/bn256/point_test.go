f3deeffc80ba72fffda8bfc53dc32a95d5f91c86
AkshayaMani <akshaya.mani@uwaterloo.ca>
2020-07-20 14:12:57 -0400

    Added regression test

--- pairing/bn256/point_test.go
+++ pairing/bn256/point_test.go
@@ -55,6 +55,41 @@ func TestPointG1_EmbedData(t *testing.T) {
 	}
 }
 
+// Regression tests for hash-to-G2
+func TestPointG2HashToPointRegression(t *testing.T) {
+	// regression test 1
+	p := new(pointG2).Hash([]byte("abc"))
+	pBuf, err := p.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	refBuf, err := hex.DecodeString("80dfe6c1dc83487bb7b72886e69934775b552f5db41fab6de00dcc9c3a59a14e836b8267e7a13e5afa904f9c011ad27de45af14c44b4dfeedf7c8e7d290dacd95f04d5463c622ce60b0c8ab9ae96d1f9ffb8d69d0207d6e3605372eb15f5a5530c0d64e7e8b6fedce3bd2993230bab11a43aec8bbb0153d461c8f9168e244c76")
+	if err != nil {
+		t.Error(err)
+	}
+	if !bytes.Equal(pBuf, refBuf) {
+		t.Error("hash does not match expected value")
+	}
+
+	// regression test 2
+	buf2, err := hex.DecodeString("e0a05cbb37fd6c159732a8c57b981773f7480695328b674d8a9cc083377f1811")
+	if err != nil {
+		t.Error(err)
+	}
+	p2 := new(pointG2).Hash(buf2)
+	p2Buf, err := p2.MarshalBinary()
+	if err != nil {
+		t.Error(err)
+	}
+	refBuf2, err := hex.DecodeString("0dfe629e88ab4afbaa36a415bad296f7329c39160b1232df1f0a2be393e19a4d0275b0223514724acf8f7f833202444da83a91e58db73eb37bd4def713e4bdf202a31171ba8753908126809fbb3ad266e959b4061755f405d12d90fbdbec8b10431ed85153b245f7788745255206c032caf8fbdd6432154a0d77dd24bc4d5937")
+	if err != nil {
+		t.Error(err)
+	}
+	if !bytes.Equal(p2Buf, refBuf2) {
+		t.Error("hash does not match expected value")
+	}
+}
+
 func testhashg2(t *testing.T, b []byte) {
 	p := new(pointG2)
 	p.Hash(b)
