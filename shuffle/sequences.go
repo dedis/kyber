5cea1f49b9a2a1bce9b30167ee5a8f31dd60176f
Noémien Kocher <nkcr.je@gmail.com>
2021-11-23 15:36:11 +0100

    Adds neff shuffling of sequences
    Based on the work from @lhmerino (https://github.com/dedis/votegral/blob/master/apps/Cothority/votegral/lib/crypto/shuffle.go)

---
+++ shuffle/sequences.go
@@ -0,0 +1,155 @@
+package shuffle
+
+import (
+	"crypto/cipher"
+	"fmt"
+
+	"go.dedis.ch/kyber/v3"
+	"go.dedis.ch/kyber/v3/proof"
+)
+
+// SequencesShuffle shuffles a sequence of ElGamal pairs based on Section 5 of
+// "Verifiable Mixing (Shuffling) of ElGamal Pairs" by Andrew Neff (April 2004)
+//
+// The function expects X and Y to be the same dimension, with each row having
+// the same length. It also expect X and Y to have at least one element.
+//
+// Dim X and Y: [<sequence length, j>, <number of sequences, i>]
+//
+// The number of rows defines the sequences length. The number of columns
+// defines the number of sequences.
+//
+//  Seq 1  Seq 2  Seq 3
+//  (0,0)  (0,1)  (0,2)
+//  (1,0)  (1,1)  (1,2)
+//  (2,0)  (2,1)  (2,2)
+//
+// In the code coordinates are (j,i), where 1 ≤ i ≤ k, 1 ≤ j ≤ NQ
+//
+// Last coordinate is (NQ-1, k-1)
+//
+// Variable names are as representative to the paper as possible. Instead of
+// representing (variable name with a bar on top), such as (X with a bar on top)
+// with Xbar, we represent it with a repeating letter, such as XX
+func SequencesShuffle(group kyber.Group, g, h kyber.Point, X, Y [][]kyber.Point,
+	rand cipher.Stream) (XX, YY [][]kyber.Point, getProver func(e []kyber.Scalar) (
+	proof.Prover, error)) {
+
+	NQ := len(X)
+	k := len(X[0])
+
+	// Pick a random permutation used in ALL k ElGamal sequences. The permutation
+	// (π) of an ElGamal pair at index i always outputs to the same index
+	pi := make([]int, k)
+	for i := 0; i < k; i++ {
+		pi[i] = i
+	}
+
+	// Fisher–Yates shuffle
+	for i := k - 1; i > 0; i-- {
+		j := int(randUint64(rand) % uint64(i+1))
+		if j != i {
+			pi[i], pi[j] = pi[j], pi[i]
+		}
+	}
+
+	// Pick a fresh ElGamal blinding factor β(j, i) for each ElGamal sequence
+	// and each ElGamal pair
+	beta := make([][]kyber.Scalar, NQ)
+	for j := 0; j < NQ; j++ {
+		beta[j] = make([]kyber.Scalar, k)
+		for i := 0; i < k; i++ {
+			beta[j][i] = group.Scalar().Pick(rand)
+		}
+	}
+
+	// Perform the Shuffle
+
+	XX = make([][]kyber.Point, NQ)
+	YY = make([][]kyber.Point, NQ)
+
+	for j := 0; j < NQ; j++ {
+		XX[j] = make([]kyber.Point, k)
+		YY[j] = make([]kyber.Point, k)
+
+		for i := 0; i < k; i++ {
+			XX[j][i] = group.Point().Mul(beta[j][pi[i]], g)
+			XX[j][i].Add(XX[j][i], X[j][pi[i]])
+
+			YY[j][i] = group.Point().Mul(beta[j][pi[i]], h)
+			YY[j][i].Add(YY[j][i], Y[j][pi[i]])
+		}
+	}
+
+	getProver = func(e []kyber.Scalar) (proof.Prover, error) {
+		// EGAR 2 (Prover) - Standard ElGamal k-shuffle proof: Knowledge of
+		// (XXUp, YYUp), (XXDown, YYDown) and e[j]
+
+		ps := PairShuffle{}
+		ps.Init(group, k)
+
+		if len(e) != NQ {
+			return nil, fmt.Errorf("len(e) must be equal to NQ: %d != %d", len(e), NQ)
+		}
+
+		return func(ctx proof.ProverContext) error {
+			// Need to consolidate beta to a one dimensional array
+			beta2 := make([]kyber.Scalar, k)
+
+			for i := 0; i < k; i++ {
+				beta2[i] = group.Scalar().Mul(e[0], beta[0][i])
+
+				for j := 1; j < NQ; j++ {
+					beta2[i] = group.Scalar().Add(beta2[i],
+						group.Scalar().Mul(e[j], beta[j][i]))
+				}
+			}
+
+			XXUp, YYUp, _, _ := GetSequenceVerifiable(group, X, Y, XX, YY, e)
+
+			return ps.Prove(pi, g, h, beta2, XXUp, YYUp, rand, ctx)
+		}, nil
+	}
+
+	return XX, YY, getProver
+}
+
+// GetSequenceVerifiable returns the consolidated input and output of sequence
+// shuffling elements. Needed by the prover and verifier.
+func GetSequenceVerifiable(group kyber.Group, X, Y, XX, YY [][]kyber.Point, e []kyber.Scalar) (
+	XXUp, YYUp, XXDown, YYDown []kyber.Point) {
+
+	// EGAR1 (Verifier) - Consolidate input and output
+
+	NQ := len(X)
+	k := len(X[0])
+
+	XXUp = make([]kyber.Point, k)
+	YYUp = make([]kyber.Point, k)
+	XXDown = make([]kyber.Point, k)
+	YYDown = make([]kyber.Point, k)
+
+	for i := 0; i < k; i++ {
+		// No modification could be made for e[0] -> e[0] = 1 if one wanted -
+		// Remark 7 in the paper
+		XXUp[i] = group.Point().Mul(e[0], X[0][i])
+		YYUp[i] = group.Point().Mul(e[0], Y[0][i])
+
+		XXDown[i] = group.Point().Mul(e[0], XX[0][i])
+		YYDown[i] = group.Point().Mul(e[0], YY[0][i])
+
+		for j := 1; j < NQ; j++ {
+			XXUp[i] = group.Point().Add(XXUp[i],
+				group.Point().Mul(e[j], X[j][i]))
+			YYUp[i] = group.Point().Add(YYUp[i],
+				group.Point().Mul(e[j], Y[j][i]))
+
+			XXDown[i] = group.Point().Add(XXDown[i],
+				group.Point().Mul(e[j], XX[j][i]))
+			YYDown[i] = group.Point().Add(YYDown[i],
+				group.Point().Mul(e[j], YY[j][i]))
+		}
+	}
+
+	return XXUp, YYUp, XXDown, YYDown
+}
--- shuffle/shuffle_test.go
+++ shuffle/shuffle_test.go
@@ -10,14 +10,20 @@ import (
 )
 
 var k = 5
+var NQ = 6
 var N = 10
 
-func TestShuffle(t *testing.T) {
+func TestShufflePair(t *testing.T) {
 	s := edwards25519.NewBlakeSHA256Ed25519WithRand(blake2xb.New(nil))
-	shuffleTest(s, k, N)
+	pairShuffleTest(s, k, N)
 }
 
-func shuffleTest(suite Suite, k, N int) {
+func TestShuffleSequence(t *testing.T) {
+	s := edwards25519.NewBlakeSHA256Ed25519WithRand(blake2xb.New(nil))
+	sequenceShuffleTest(s, k, NQ, N)
+}
+
+func pairShuffleTest(suite Suite, k, N int) {
 	rand := suite.RandomStream()
 
 	// Create a "server" private/public keypair
@@ -61,6 +67,85 @@ func shuffleTest(suite Suite, k, N int) {
 		err = proof.HashVerify(suite, "PairShuffle", verifier, prf)
 		if err != nil {
 			panic("Shuffle verify failed: " + err.Error())
+		}
+	}
+}
+
+func sequenceShuffleTest(suite Suite, k, NQ, N int) {
+	rand := suite.RandomStream()
+
+	// Create a "server" private/public keypair
+	h := suite.Scalar().Pick(rand)
+	H := suite.Point().Mul(h, nil)
+
+	// Create a set of ephemeral "client" keypairs to shuffle
+	c := make([]kyber.Scalar, k)
+	C := make([]kyber.Point, k)
+
+	for i := 0; i < k; i++ {
+		c[i] = suite.Scalar().Pick(rand)
+		C[i] = suite.Point().Mul(c[i], nil)
+		//		fmt.Println(" "+C[i].String())
+	}
+
+	X := make([][]kyber.Point, NQ)
+	Y := make([][]kyber.Point, NQ)
+
+	// generate random sequences
+
+	for i := 0; i < NQ; i++ {
+		xs := make([]kyber.Point, k)
+		ys := make([]kyber.Point, k)
+
+		for i := 0; i < k; i++ {
+			xs[i] = suite.Point().Mul(suite.Scalar().Pick(suite.RandomStream()), nil)
+			ys[i] = suite.Point().Mul(suite.Scalar().Pick(suite.RandomStream()), nil)
+		}
+
+		X[i] = xs
+		Y[i] = ys
+	}
+
+	// ElGamal-encrypt all these keypairs with the "server" key
+	r := suite.Scalar() // temporary
+	for j := 0; j < NQ; j++ {
+		for i := 0; i < k; i++ {
+			r.Pick(rand)
+			X[j][i] = suite.Point().Mul(r, nil)
+			Y[j][i] = suite.Point().Mul(r, H) // ElGamal blinding factor
+			Y[j][i].Add(Y[j][i], C[i])        // Encrypted client public key
+		}
+	}
+
+	// Repeat only the actual shuffle portion for test purposes.
+	for i := 0; i < N; i++ {
+
+		// Do a key-shuffle
+		XX, YY, getProver := SequencesShuffle(suite, nil, H, X, Y, rand)
+
+		e := make([]kyber.Scalar, NQ)
+		for j := 0; j < NQ; j++ {
+			e[j] = suite.Scalar().Pick(suite.RandomStream())
+		}
+
+		prover, err := getProver(e)
+		if err != nil {
+			panic("failed to get prover: " + err.Error())
+		}
+
+		prf, err := proof.HashProve(suite, "PairShuffle", prover)
+		if err != nil {
+			panic("failed to hashProve: " + err.Error())
+		}
+
+		XXUp, YYUp, XXDown, YYDown := GetSequenceVerifiable(suite, X, Y, XX, YY, e)
+
+		// Check it
+		verifier := Verifier(suite, nil, H, XXUp, YYUp, XXDown, YYDown)
+
+		err = proof.HashVerify(suite, "PairShuffle", verifier, prf)
+		if err != nil {
+			panic("failed to hashVerify: " + err.Error())
 		}
 	}
 }
