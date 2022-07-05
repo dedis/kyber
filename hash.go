a4d8acbfa35dfee7a9648c1c24bdb08f2719a061
PM <3749956+CluEleSsUK@users.noreply.github.com>
2022-07-05 11:35:05 +0200

    Identity-based encryption features to support timelock encryption (#28)
    migrated from the `timelock` branch where work was done previously,
    but stripped out the aggregation code and added a few tests
    
    Co-authored-by: nikkolasg <nikkolasg@gmail.com>

--- hash.go
+++ hash.go
@@ -1,8 +1,15 @@
 package kyber
 
-import "hash"
+import (
+	"hash"
+)
 
 // A HashFactory is an interface that can be mixed in to local suite definitions.
 type HashFactory interface {
 	Hash() hash.Hash
+}
+
+// HashablePoint is an interface implemented by n
+type HashablePoint interface {
+	Hash([]byte) Point
 }
