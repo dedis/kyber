38b49c629e876f7b0009226fe4788bbe029d2dd7
matteosz <mattsuez01@gmail.com>
2024-02-15 10:39:21 +0100

    Increased test coverage for util/random and util/encoding

--- util/encoding/encoding_test.go
+++ util/encoding/encoding_test.go
@@ -2,6 +2,7 @@ package encoding
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -59,4 +60,41 @@ func TestScalarHexString(t *testing.T) {
 	s2, err := StringHexToScalar(s, scstr)
 	ErrFatal(err)
 	require.True(t, sc.Equal(s2))
+}
+
+// Tests for error cases
+type MockFailingReader struct {
+	data []byte
+}
+type MockEmptyReader struct {
+	data []byte
+}
+
+func (m *MockFailingReader) Read(p []byte) (n int, err error) {
+	return copy(p, m.data), io.EOF
+}
+func (m *MockEmptyReader) Read(p []byte) (n int, err error) {
+	return 0, nil
+}
+
+func TestReadHexPointErrorInvalidHexEnc(t *testing.T) {
+	// Test case: invalid hex encoding
+	reader := bytes.NewReader([]byte("invalidhex"))
+	_, err := ReadHexPoint(s, reader)
+	require.Error(t, err, "Expected error when reading invalid hex encoding, but got nil")
+}
+
+func TestReadHexPointErrorReaderFails(t *testing.T) {
+	// Test case: reader fails
+	mockReader1 := &MockFailingReader{data: []byte("abc")}
+	_, err := ReadHexPoint(s, mockReader1)
+	require.Error(t, err, "Expected error when reader fails, but got nil")
+}
+
+func TestReadHexPointErrorNotEnoughBytes(t *testing.T) {
+	// Test case: not enough bytes from stream
+	mockReader2 := &MockEmptyReader{data: []byte("abc")}
+	_, err := ReadHexPoint(s, mockReader2)
+	require.Error(t, err, "Expected error when not enough bytes from stream, but got nil")
+	require.EqualError(t, err, "didn't get enough bytes from stream", "Expected error message: didn't get enough bytes from stream, but got %s", err.Error())
 }
