e58a5ff36ba6d9b633dd8edf00196b20aa4250bc
lauener <kilian.lauener@epfl.ch>
2024-02-22 16:23:21 +0100

    Fix lint issues package curve25519

--- group/curve25519/ext.go
+++ group/curve25519/ext.go
@@ -16,50 +16,50 @@ type extPoint struct {
 	c          *ExtendedCurve
 }
 
-func (P *extPoint) initXY(x, y *big.Int, c kyber.Group) {
-	P.c = c.(*ExtendedCurve)
-	P.X.Init(x, &P.c.P)
-	P.Y.Init(y, &P.c.P)
-	P.Z.Init64(1, &P.c.P)
-	P.T.Mul(&P.X, &P.Y)
+func (p *extPoint) initXY(x, y *big.Int, c kyber.Group) {
+	p.c = c.(*ExtendedCurve)
+	p.X.Init(x, &p.c.P)
+	p.Y.Init(y, &p.c.P)
+	p.Z.Init64(1, &p.c.P)
+	p.T.Mul(&p.X, &p.Y)
 }
 
-func (P *extPoint) getXY() (x, y *mod.Int) {
-	P.normalize()
-	return &P.X, &P.Y
+func (p *extPoint) getXY() (x, y *mod.Int) {
+	p.normalize()
+	return &p.X, &p.Y
 }
 
-func (P *extPoint) String() string {
-	P.normalize()
-	//return P.c.pointString(&P.X,&P.Y)
-	buf, _ := P.MarshalBinary()
+func (p *extPoint) String() string {
+	p.normalize()
+	//return p.c.pointString(&p.X,&p.Y)
+	buf, _ := p.MarshalBinary()
 	return hex.EncodeToString(buf)
 }
 
-func (P *extPoint) MarshalSize() int {
-	return P.c.PointLen()
+func (p *extPoint) MarshalSize() int {
+	return p.c.PointLen()
 }
 
-func (P *extPoint) MarshalBinary() ([]byte, error) {
-	P.normalize()
-	return P.c.encodePoint(&P.X, &P.Y), nil
+func (p *extPoint) MarshalBinary() ([]byte, error) {
+	p.normalize()
+	return p.c.encodePoint(&p.X, &p.Y), nil
 }
 
-func (P *extPoint) UnmarshalBinary(b []byte) error {
-	if err := P.c.decodePoint(b, &P.X, &P.Y); err != nil {
+func (p *extPoint) UnmarshalBinary(b []byte) error {
+	if err := p.c.decodePoint(b, &p.X, &p.Y); err != nil {
 		return err
 	}
-	P.Z.Init64(1, &P.c.P)
-	P.T.Mul(&P.X, &P.Y)
+	p.Z.Init64(1, &p.c.P)
+	p.T.Mul(&p.X, &p.Y)
 	return nil
 }
 
-func (P *extPoint) MarshalTo(w io.Writer) (int, error) {
-	return marshalling.PointMarshalTo(P, w)
+func (p *extPoint) MarshalTo(w io.Writer) (int, error) {
+	return marshalling.PointMarshalTo(p, w)
 }
 
-func (P *extPoint) UnmarshalFrom(r io.Reader) (int, error) {
-	return marshalling.PointUnmarshalFrom(P, r)
+func (p *extPoint) UnmarshalFrom(r io.Reader) (int, error) {
+	return marshalling.PointUnmarshalFrom(p, r)
 }
 
 // Equality test for two Points on the same curve.
@@ -68,152 +68,156 @@ func (P *extPoint) UnmarshalFrom(r io.Reader) (int, error) {
 //	(X1/Z1,Y1/Z1) == (X2/Z2,Y2/Z2)
 //		iff
 //	(X1*Z2,Y1*Z2) == (X2*Z1,Y2*Z1)
-func (P *extPoint) Equal(CP2 kyber.Point) bool {
-	P2 := CP2.(*extPoint)
+func (p *extPoint) Equal(cp2 kyber.Point) bool {
+	p2 := cp2.(*extPoint)
 	var t1, t2 mod.Int
-	xeq := t1.Mul(&P.X, &P2.Z).Equal(t2.Mul(&P2.X, &P.Z))
-	yeq := t1.Mul(&P.Y, &P2.Z).Equal(t2.Mul(&P2.Y, &P.Z))
+	xeq := t1.Mul(&p.X, &p2.Z).Equal(t2.Mul(&p2.X, &p.Z))
+	yeq := t1.Mul(&p.Y, &p2.Z).Equal(t2.Mul(&p2.Y, &p.Z))
 	return xeq && yeq
 }
 
-func (P *extPoint) Set(CP2 kyber.Point) kyber.Point {
-	P2 := CP2.(*extPoint)
-	P.c = P2.c
-	P.X.Set(&P2.X)
-	P.Y.Set(&P2.Y)
-	P.Z.Set(&P2.Z)
-	P.T.Set(&P2.T)
-	return P
+func (p *extPoint) Set(cp2 kyber.Point) kyber.Point {
+	p2 := cp2.(*extPoint)
+	p.c = p2.c
+	p.X.Set(&p2.X)
+	p.Y.Set(&p2.Y)
+	p.Z.Set(&p2.Z)
+	p.T.Set(&p2.T)
+	return p
 }
 
-func (P *extPoint) Clone() kyber.Point {
-	P2 := extPoint{}
-	P2.c = P.c
-	P2.X.Set(&P.X)
-	P2.Y.Set(&P.Y)
-	P2.Z.Set(&P.Z)
-	P2.T.Set(&P.T)
-	return &P2
+func (p *extPoint) Clone() kyber.Point {
+	p2 := extPoint{}
+	p2.c = p.c
+	p2.X.Set(&p.X)
+	p2.Y.Set(&p.Y)
+	p2.Z.Set(&p.Z)
+	p2.T.Set(&p.T)
+	return &p2
 }
 
-func (P *extPoint) Null() kyber.Point {
-	P.Set(&P.c.null)
-	return P
+func (p *extPoint) Null() kyber.Point {
+	p.Set(&p.c.null)
+	return p
 }
 
-func (P *extPoint) Base() kyber.Point {
-	P.Set(&P.c.base)
-	return P
+func (p *extPoint) Base() kyber.Point {
+	p.Set(&p.c.base)
+	return p
 }
 
-func (P *extPoint) EmbedLen() int {
-	return P.c.embedLen()
+func (p *extPoint) EmbedLen() int {
+	return p.c.embedLen()
 }
 
 // Normalize the point's representation to Z=1.
-func (P *extPoint) normalize() {
-	P.Z.Inv(&P.Z)
-	P.X.Mul(&P.X, &P.Z)
-	P.Y.Mul(&P.Y, &P.Z)
-	P.Z.V.SetInt64(1)
-	P.T.Mul(&P.X, &P.Y)
+func (p *extPoint) normalize() {
+	p.Z.Inv(&p.Z)
+	p.X.Mul(&p.X, &p.Z)
+	p.Y.Mul(&p.Y, &p.Z)
+	p.Z.V.SetInt64(1)
+	p.T.Mul(&p.X, &p.Y)
 }
 
 // Check the validity of the T coordinate
-func (P *extPoint) checkT() {
+func (p *extPoint) checkT() {
 	var t1, t2 mod.Int
-	if !t1.Mul(&P.X, &P.Y).Equal(t2.Mul(&P.Z, &P.T)) {
+	if !t1.Mul(&p.X, &p.Y).Equal(t2.Mul(&p.Z, &p.T)) {
 		panic("oops")
 	}
 }
 
-func (P *extPoint) Embed(data []byte, rand cipher.Stream) kyber.Point {
-	P.c.embed(P, data, rand)
-	return P
+func (p *extPoint) Embed(data []byte, rand cipher.Stream) kyber.Point {
+	p.c.embed(p, data, rand)
+	return p
 }
 
-func (P *extPoint) Pick(rand cipher.Stream) kyber.Point {
-	P.c.embed(P, nil, rand)
-	return P
+func (p *extPoint) Pick(rand cipher.Stream) kyber.Point {
+	p.c.embed(p, nil, rand)
+	return p
 }
 
 // Extract embedded data from a point group element
-func (P *extPoint) Data() ([]byte, error) {
-	P.normalize()
-	return P.c.data(&P.X, &P.Y)
+func (p *extPoint) Data() ([]byte, error) {
+	p.normalize()
+	return p.c.data(&p.X, &p.Y)
 }
 
 // Add two points using optimized extended coordinate addition formulas.
-func (P *extPoint) Add(CP1, CP2 kyber.Point) kyber.Point {
-	P1 := CP1.(*extPoint)
-	P2 := CP2.(*extPoint)
-	X1, Y1, Z1, T1 := &P1.X, &P1.Y, &P1.Z, &P1.T
-	X2, Y2, Z2, T2 := &P2.X, &P2.Y, &P2.Z, &P2.T
-	X3, Y3, Z3, T3 := &P.X, &P.Y, &P.Z, &P.T
+//
+//nolint:dupl //Doesn't make sense to extract part of Add(), Sub(), double()
+func (p *extPoint) Add(cp1, cp2 kyber.Point) kyber.Point {
+	p1 := cp1.(*extPoint)
+	p2 := cp2.(*extPoint)
+	X1, Y1, Z1, T1 := &p1.X, &p1.Y, &p1.Z, &p1.T
+	X2, Y2, Z2, T2 := &p2.X, &p2.Y, &p2.Z, &p2.T
+	X3, Y3, Z3, T3 := &p.X, &p.Y, &p.Z, &p.T
 	var A, B, C, D, E, F, G, H mod.Int
 
 	A.Mul(X1, X2)
 	B.Mul(Y1, Y2)
-	C.Mul(T1, T2).Mul(&C, &P.c.d)
+	C.Mul(T1, T2).Mul(&C, &p.c.d)
 	D.Mul(Z1, Z2)
 	E.Add(X1, Y1).Mul(&E, F.Add(X2, Y2)).Sub(&E, &A).Sub(&E, &B)
 	F.Sub(&D, &C)
 	G.Add(&D, &C)
-	H.Mul(&P.c.a, &A).Sub(&B, &H)
+	H.Mul(&p.c.a, &A).Sub(&B, &H)
 	X3.Mul(&E, &F)
 	Y3.Mul(&G, &H)
 	T3.Mul(&E, &H)
 	Z3.Mul(&F, &G)
-	return P
+	return p
 }
 
 // Subtract points.
-func (P *extPoint) Sub(CP1, CP2 kyber.Point) kyber.Point {
-	P1 := CP1.(*extPoint)
-	P2 := CP2.(*extPoint)
-	X1, Y1, Z1, T1 := &P1.X, &P1.Y, &P1.Z, &P1.T
-	X2, Y2, Z2, T2 := &P2.X, &P2.Y, &P2.Z, &P2.T
-	X3, Y3, Z3, T3 := &P.X, &P.Y, &P.Z, &P.T
+//
+//nolint:dupl //Doesn't make sense to extract part of Add(), Sub(), double()
+func (p *extPoint) Sub(cp1, cp2 kyber.Point) kyber.Point {
+	p1 := cp1.(*extPoint)
+	p2 := cp2.(*extPoint)
+	X1, Y1, Z1, T1 := &p1.X, &p1.Y, &p1.Z, &p1.T
+	X2, Y2, Z2, T2 := &p2.X, &p2.Y, &p2.Z, &p2.T
+	X3, Y3, Z3, T3 := &p.X, &p.Y, &p.Z, &p.T
 	var A, B, C, D, E, F, G, H mod.Int
 
 	A.Mul(X1, X2)
 	B.Mul(Y1, Y2)
-	C.Mul(T1, T2).Mul(&C, &P.c.d)
+	C.Mul(T1, T2).Mul(&C, &p.c.d)
 	D.Mul(Z1, Z2)
 	E.Add(X1, Y1).Mul(&E, F.Sub(Y2, X2)).Add(&E, &A).Sub(&E, &B)
 	F.Add(&D, &C)
 	G.Sub(&D, &C)
-	H.Mul(&P.c.a, &A).Add(&B, &H)
+	H.Mul(&p.c.a, &A).Add(&B, &H)
 	X3.Mul(&E, &F)
 	Y3.Mul(&G, &H)
 	T3.Mul(&E, &H)
 	Z3.Mul(&F, &G)
-	return P
+	return p
 }
 
 // Find the negative of point A.
 // For Edwards curves, the negative of (x,y) is (-x,y).
-func (P *extPoint) Neg(CA kyber.Point) kyber.Point {
-	A := CA.(*extPoint)
-	P.c = A.c
-	P.X.Neg(&A.X)
-	P.Y.Set(&A.Y)
-	P.Z.Set(&A.Z)
-	P.T.Neg(&A.T)
-	return P
+func (p *extPoint) Neg(ca kyber.Point) kyber.Point {
+	A := ca.(*extPoint)
+	p.c = A.c
+	p.X.Neg(&A.X)
+	p.Y.Set(&A.Y)
+	p.Z.Set(&A.Z)
+	p.T.Neg(&A.T)
+	return p
 }
 
 // Optimized point doubling for use in scalar multiplication.
 // Uses the formulae in section 3.3 of:
 // https://www.iacr.org/archive/asiacrypt2008/53500329/53500329.pdf
-func (P *extPoint) double() {
-	X1, Y1, Z1, T1 := &P.X, &P.Y, &P.Z, &P.T
+func (p *extPoint) double() {
+	X1, Y1, Z1, T1 := &p.X, &p.Y, &p.Z, &p.T
 	var A, B, C, D, E, F, G, H mod.Int
 
 	A.Mul(X1, X1)
 	B.Mul(Y1, Y1)
 	C.Mul(Z1, Z1).Add(&C, &C)
-	D.Mul(&P.c.a, &A)
+	D.Mul(&p.c.a, &A)
 	E.Add(X1, Y1).Mul(&E, &E).Sub(&E, &A).Sub(&E, &B)
 	G.Add(&D, &B)
 	F.Sub(&G, &C)
@@ -229,26 +233,26 @@ func (P *extPoint) double() {
 // Currently doesn't implement the optimization of
 // switching between projective and extended coordinates during
 // scalar multiplication.
-func (P *extPoint) Mul(s kyber.Scalar, G kyber.Point) kyber.Point {
+func (p *extPoint) Mul(s kyber.Scalar, g kyber.Point) kyber.Point {
 	v := s.(*mod.Int).V
-	if G == nil {
-		return P.Base().Mul(s, P)
+	if g == nil {
+		return p.Base().Mul(s, p)
 	}
-	T := P
-	if G == P { // Must use temporary for in-place multiply
+	T := p
+	if g == p { // Must use temporary for in-place multiply
 		T = &extPoint{}
 	}
-	T.Set(&P.c.null) // Initialize to identity element (0,1)
+	T.Set(&p.c.null) // Initialize to identity element (0,1)
 	for i := v.BitLen() - 1; i >= 0; i-- {
 		T.double()
 		if v.Bit(i) != 0 {
-			T.Add(T, G)
+			T.Add(T, g)
 		}
 	}
-	if T != P {
-		P.Set(T)
+	if T != p {
+		p.Set(T)
 	}
-	return P
+	return p
 }
 
 // ExtendedCurve implements Twisted Edwards curves
--- group/curve25519/proj.go
+++ group/curve25519/proj.go
@@ -15,43 +15,43 @@ type projPoint struct {
 	c       *ProjectiveCurve
 }
 
-func (P *projPoint) initXY(x, y *big.Int, c kyber.Group) {
-	P.c = c.(*ProjectiveCurve)
-	P.X.Init(x, &P.c.P)
-	P.Y.Init(y, &P.c.P)
-	P.Z.Init64(1, &P.c.P)
+func (p *projPoint) initXY(x, y *big.Int, c kyber.Group) {
+	p.c = c.(*ProjectiveCurve)
+	p.X.Init(x, &p.c.P)
+	p.Y.Init(y, &p.c.P)
+	p.Z.Init64(1, &p.c.P)
 }
 
-func (P *projPoint) getXY() (x, y *mod.Int) {
-	P.normalize()
-	return &P.X, &P.Y
+func (p *projPoint) getXY() (x, y *mod.Int) {
+	p.normalize()
+	return &p.X, &p.Y
 }
 
-func (P *projPoint) String() string {
-	P.normalize()
-	return P.c.pointString(&P.X, &P.Y)
+func (p *projPoint) String() string {
+	p.normalize()
+	return p.c.pointString(&p.X, &p.Y)
 }
 
-func (P *projPoint) MarshalSize() int {
-	return P.c.PointLen()
+func (p *projPoint) MarshalSize() int {
+	return p.c.PointLen()
 }
 
-func (P *projPoint) MarshalBinary() ([]byte, error) {
-	P.normalize()
-	return P.c.encodePoint(&P.X, &P.Y), nil
+func (p *projPoint) MarshalBinary() ([]byte, error) {
+	p.normalize()
+	return p.c.encodePoint(&p.X, &p.Y), nil
 }
 
-func (P *projPoint) UnmarshalBinary(b []byte) error {
-	P.Z.Init64(1, &P.c.P)
-	return P.c.decodePoint(b, &P.X, &P.Y)
+func (p *projPoint) UnmarshalBinary(b []byte) error {
+	p.Z.Init64(1, &p.c.P)
+	return p.c.decodePoint(b, &p.X, &p.Y)
 }
 
-func (P *projPoint) MarshalTo(w io.Writer) (int, error) {
-	return marshalling.PointMarshalTo(P, w)
+func (p *projPoint) MarshalTo(w io.Writer) (int, error) {
+	return marshalling.PointMarshalTo(p, w)
 }
 
-func (P *projPoint) UnmarshalFrom(r io.Reader) (int, error) {
-	return marshalling.PointUnmarshalFrom(P, r)
+func (p *projPoint) UnmarshalFrom(r io.Reader) (int, error) {
+	return marshalling.PointUnmarshalFrom(p, r)
 }
 
 // Equality test for two Points on the same curve.
@@ -60,67 +60,67 @@ func (P *projPoint) UnmarshalFrom(r io.Reader) (int, error) {
 //	(X1/Z1,Y1/Z1) == (X2/Z2,Y2/Z2)
 //		iff
 //	(X1*Z2,Y1*Z2) == (X2*Z1,Y2*Z1)
-func (P *projPoint) Equal(CP2 kyber.Point) bool {
-	P2 := CP2.(*projPoint)
+func (p *projPoint) Equal(cp2 kyber.Point) bool {
+	P2 := cp2.(*projPoint)
 	var t1, t2 mod.Int
-	xeq := t1.Mul(&P.X, &P2.Z).Equal(t2.Mul(&P2.X, &P.Z))
-	yeq := t1.Mul(&P.Y, &P2.Z).Equal(t2.Mul(&P2.Y, &P.Z))
+	xeq := t1.Mul(&p.X, &P2.Z).Equal(t2.Mul(&P2.X, &p.Z))
+	yeq := t1.Mul(&p.Y, &P2.Z).Equal(t2.Mul(&P2.Y, &p.Z))
 	return xeq && yeq
 }
 
-func (P *projPoint) Set(CP2 kyber.Point) kyber.Point {
-	P2 := CP2.(*projPoint)
-	P.c = P2.c
-	P.X.Set(&P2.X)
-	P.Y.Set(&P2.Y)
-	P.Z.Set(&P2.Z)
-	return P
+func (p *projPoint) Set(cp2 kyber.Point) kyber.Point {
+	P2 := cp2.(*projPoint)
+	p.c = P2.c
+	p.X.Set(&P2.X)
+	p.Y.Set(&P2.Y)
+	p.Z.Set(&P2.Z)
+	return p
 }
 
-func (P *projPoint) Clone() kyber.Point {
+func (p *projPoint) Clone() kyber.Point {
 	P2 := projPoint{}
-	P2.c = P.c
-	P2.X.Set(&P.X)
-	P2.Y.Set(&P.Y)
-	P2.Z.Set(&P.Z)
+	P2.c = p.c
+	P2.X.Set(&p.X)
+	P2.Y.Set(&p.Y)
+	P2.Z.Set(&p.Z)
 	return &P2
 }
 
-func (P *projPoint) Null() kyber.Point {
-	P.Set(&P.c.null)
-	return P
+func (p *projPoint) Null() kyber.Point {
+	p.Set(&p.c.null)
+	return p
 }
 
-func (P *projPoint) Base() kyber.Point {
-	P.Set(&P.c.base)
-	return P
+func (p *projPoint) Base() kyber.Point {
+	p.Set(&p.c.base)
+	return p
 }
 
-func (P *projPoint) EmbedLen() int {
-	return P.c.embedLen()
+func (p *projPoint) EmbedLen() int {
+	return p.c.embedLen()
 }
 
 // Normalize the point's representation to Z=1.
-func (P *projPoint) normalize() {
-	P.Z.Inv(&P.Z)
-	P.X.Mul(&P.X, &P.Z)
-	P.Y.Mul(&P.Y, &P.Z)
-	P.Z.V.SetInt64(1)
+func (p *projPoint) normalize() {
+	p.Z.Inv(&p.Z)
+	p.X.Mul(&p.X, &p.Z)
+	p.Y.Mul(&p.Y, &p.Z)
+	p.Z.V.SetInt64(1)
 }
 
-func (P *projPoint) Embed(data []byte, rand cipher.Stream) kyber.Point {
-	P.c.embed(P, data, rand)
-	return P
+func (p *projPoint) Embed(data []byte, rand cipher.Stream) kyber.Point {
+	p.c.embed(p, data, rand)
+	return p
 }
 
-func (P *projPoint) Pick(rand cipher.Stream) kyber.Point {
-	return P.Embed(nil, rand)
+func (p *projPoint) Pick(rand cipher.Stream) kyber.Point {
+	return p.Embed(nil, rand)
 }
 
 // Extract embedded data from a point group element
-func (P *projPoint) Data() ([]byte, error) {
-	P.normalize()
-	return P.c.data(&P.X, &P.Y)
+func (p *projPoint) Data() ([]byte, error) {
+	p.normalize()
+	return p.c.data(&p.X, &p.Y)
 }
 
 // Add two points using optimized projective coordinate addition formulas.
@@ -128,9 +128,11 @@ func (P *projPoint) Data() ([]byte, error) {
 //
 //	http://eprint.iacr.org/2008/013.pdf
 //	https://hyperelliptic.org/EFD/g1p/auto-twisted-projective.html
-func (P *projPoint) Add(CP1, CP2 kyber.Point) kyber.Point {
-	P1 := CP1.(*projPoint)
-	P2 := CP2.(*projPoint)
+//
+//nolint:dupl //Doesn't make sense to extract part of Add(), Sub()
+func (p *projPoint) Add(cp1, cp2 kyber.Point) kyber.Point {
+	P1 := cp1.(*projPoint)
+	P2 := cp2.(*projPoint)
 	X1, Y1, Z1 := &P1.X, &P1.Y, &P1.Z
 	X2, Y2, Z2 := &P2.X, &P2.Y, &P2.Z
 	var A, B, C, D, E, F, G, X3, Y3, Z3 mod.Int
@@ -139,25 +141,27 @@ func (P *projPoint) Add(CP1, CP2 kyber.Point) kyber.Point {
 	B.Mul(&A, &A)
 	C.Mul(X1, X2)
 	D.Mul(Y1, Y2)
-	E.Mul(&C, &D).Mul(&P.c.d, &E)
+	E.Mul(&C, &D).Mul(&p.c.d, &E)
 	F.Sub(&B, &E)
 	G.Add(&B, &E)
 	X3.Add(X1, Y1).Mul(&X3, Z3.Add(X2, Y2)).Sub(&X3, &C).Sub(&X3, &D).
 		Mul(&F, &X3).Mul(&A, &X3)
-	Y3.Mul(&P.c.a, &C).Sub(&D, &Y3).Mul(&G, &Y3).Mul(&A, &Y3)
+	Y3.Mul(&p.c.a, &C).Sub(&D, &Y3).Mul(&G, &Y3).Mul(&A, &Y3)
 	Z3.Mul(&F, &G)
 
-	P.c = P1.c
-	P.X.Set(&X3)
-	P.Y.Set(&Y3)
-	P.Z.Set(&Z3)
-	return P
+	p.c = P1.c
+	p.X.Set(&X3)
+	p.Y.Set(&Y3)
+	p.Z.Set(&Z3)
+	return p
 }
 
 // Subtract points so that their scalars subtract homomorphically
-func (P *projPoint) Sub(CP1, CP2 kyber.Point) kyber.Point {
-	P1 := CP1.(*projPoint)
-	P2 := CP2.(*projPoint)
+//
+//nolint:dupl //Doesn't make sense to extract part of Add(), Sub(), double()
+func (p *projPoint) Sub(cp1, cp2 kyber.Point) kyber.Point {
+	P1 := cp1.(*projPoint)
+	P2 := cp2.(*projPoint)
 	X1, Y1, Z1 := &P1.X, &P1.Y, &P1.Z
 	X2, Y2, Z2 := &P2.X, &P2.Y, &P2.Z
 	var A, B, C, D, E, F, G, X3, Y3, Z3 mod.Int
@@ -166,69 +170,69 @@ func (P *projPoint) Sub(CP1, CP2 kyber.Point) kyber.Point {
 	B.Mul(&A, &A)
 	C.Mul(X1, X2)
 	D.Mul(Y1, Y2)
-	E.Mul(&C, &D).Mul(&P.c.d, &E)
+	E.Mul(&C, &D).Mul(&p.c.d, &E)
 	F.Add(&B, &E)
 	G.Sub(&B, &E)
 	X3.Add(X1, Y1).Mul(&X3, Z3.Sub(Y2, X2)).Add(&X3, &C).Sub(&X3, &D).
 		Mul(&F, &X3).Mul(&A, &X3)
-	Y3.Mul(&P.c.a, &C).Add(&D, &Y3).Mul(&G, &Y3).Mul(&A, &Y3)
+	Y3.Mul(&p.c.a, &C).Add(&D, &Y3).Mul(&G, &Y3).Mul(&A, &Y3)
 	Z3.Mul(&F, &G)
 
-	P.c = P1.c
-	P.X.Set(&X3)
-	P.Y.Set(&Y3)
-	P.Z.Set(&Z3)
-	return P
+	p.c = P1.c
+	p.X.Set(&X3)
+	p.Y.Set(&Y3)
+	p.Z.Set(&Z3)
+	return p
 }
 
 // Find the negative of point A.
 // For Edwards curves, the negative of (x,y) is (-x,y).
-func (P *projPoint) Neg(CA kyber.Point) kyber.Point {
-	A := CA.(*projPoint)
-	P.c = A.c
-	P.X.Neg(&A.X)
-	P.Y.Set(&A.Y)
-	P.Z.Set(&A.Z)
-	return P
+func (p *projPoint) Neg(ca kyber.Point) kyber.Point {
+	A := ca.(*projPoint)
+	p.c = A.c
+	p.X.Neg(&A.X)
+	p.Y.Set(&A.Y)
+	p.Z.Set(&A.Z)
+	return p
 }
 
 // Optimized point doubling for use in scalar multiplication.
-func (P *projPoint) double() {
+func (p *projPoint) double() {
 	var B, C, D, E, F, H, J mod.Int
 
-	B.Add(&P.X, &P.Y).Mul(&B, &B)
-	C.Mul(&P.X, &P.X)
-	D.Mul(&P.Y, &P.Y)
-	E.Mul(&P.c.a, &C)
+	B.Add(&p.X, &p.Y).Mul(&B, &B)
+	C.Mul(&p.X, &p.X)
+	D.Mul(&p.Y, &p.Y)
+	E.Mul(&p.c.a, &C)
 	F.Add(&E, &D)
-	H.Mul(&P.Z, &P.Z)
+	H.Mul(&p.Z, &p.Z)
 	J.Add(&H, &H).Sub(&F, &J)
-	P.X.Sub(&B, &C).Sub(&P.X, &D).Mul(&P.X, &J)
-	P.Y.Sub(&E, &D).Mul(&F, &P.Y)
-	P.Z.Mul(&F, &J)
+	p.X.Sub(&B, &C).Sub(&p.X, &D).Mul(&p.X, &J)
+	p.Y.Sub(&E, &D).Mul(&F, &p.Y)
+	p.Z.Mul(&F, &J)
 }
 
 // Multiply point p by scalar s using the repeated doubling method.
-func (P *projPoint) Mul(s kyber.Scalar, G kyber.Point) kyber.Point {
+func (p *projPoint) Mul(s kyber.Scalar, g kyber.Point) kyber.Point {
 	v := s.(*mod.Int).V
-	if G == nil {
-		return P.Base().Mul(s, P)
+	if g == nil {
+		return p.Base().Mul(s, p)
 	}
-	T := P
-	if G == P { // Must use temporary for in-place multiply
+	T := p
+	if g == p { // Must use temporary for in-place multiply
 		T = &projPoint{}
 	}
-	T.Set(&P.c.null) // Initialize to identity element (0,1)
+	T.Set(&p.c.null) // Initialize to identity element (0,1)
 	for i := v.BitLen() - 1; i >= 0; i-- {
 		T.double()
 		if v.Bit(i) != 0 {
-			T.Add(T, G)
+			T.Add(T, g)
 		}
 	}
-	if T != P {
-		P.Set(T)
+	if T != p {
+		p.Set(T)
 	}
-	return P
+	return p
 }
 
 // ProjectiveCurve implements Twisted Edwards curves
