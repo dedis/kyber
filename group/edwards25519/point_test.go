8d120b8d5e1f8f0f50b6ebaf4d94a8a7c5b2c3f0
lauener <kilian.lauener@epfl.ch>
2024-04-15 07:55:57 +0200

    Add test vector sha512

--- group/edwards25519/point_test.go
+++ group/edwards25519/point_test.go
@@ -2,6 +2,7 @@ package edwards25519
 
 import (
 	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/hex"
 	"fmt"
 	"testing"
@@ -106,6 +107,66 @@ func Test_ExpandMessageXMDSHA256(t *testing.T) {
 	}
 
 	h := sha256.New()
+
+	// Short
+	for i := 0; i < len(inputs); i++ {
+		res, err := expandMessageXMD(h, []byte(inputs[i]), dst, outputLength[0])
+		resHex := hex.EncodeToString(res)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedHex32byte[i], resHex)
+	}
+
+	// Long
+	for i := 0; i < len(inputs); i++ {
+		res, err := expandMessageXMD(h, []byte(inputs[i]), dst, outputLength[1])
+		resHex := hex.EncodeToString(res)
+
+		assert.NoError(t, err)
+		assert.Equal(t, expectedHex128byte[i], resHex)
+	}
+}
+
+// Test vectors from: https://datatracker.ietf.org/doc/rfc9380
+func Test_ExpandMessageXMDSHA512(t *testing.T) {
+	dst := "QUUX-V01-CS02-with-expander-SHA512-256"
+	h := sha512.New()
+	inputs := []string{
+		"",
+		"abc",
+		"abcdef0123456789",
+		"q128_qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq" +
+			"qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq" +
+			"qqqqqqqqqqqqqqqqqqqqqqqqq",
+		"a512_aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" +
+			"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
+	}
+
+	outputLength := []int{32, 128}
+
+	expectedHex32byte := []string{
+		"6b9a7312411d92f921c6f68ca0b6380730a1a4d982c507211a90964c394179ba",
+		"0da749f12fbe5483eb066a5f595055679b976e93abe9be6f0f6318bce7aca8dc",
+		"087e45a86e2939ee8b91100af1583c4938e0f5fc6c9db4b107b83346bc967f58",
+		"7336234ee9983902440f6bc35b348352013becd88938d2afec44311caf8356b3",
+		"57b5f7e766d5be68a6bfe1768e3c2b7f1228b3e4b3134956dd73a59b954c66f4",
+	}
+
+	expectedHex128byte := []string{
+		"41b037d1734a5f8df225dd8c7de38f851efdb45c372887be655212d07251b921b052b62eaed99b46f72f2ef4cc96bfaf254ebbbec091e1a3b9e4fb5e5b619d2e0c5414800a1d882b62bb5cd1778f098b8eb6cb399d5d9d18f5d5842cf5d13d7eb00a7cff859b605da678b318bd0e65ebff70bec88c753b159a805d2c89c55961",
+		"7f1dddd13c08b543f2e2037b14cefb255b44c83cc397c1786d975653e36a6b11bdd7732d8b38adb4a0edc26a0cef4bb45217135456e58fbca1703cd6032cb1347ee720b87972d63fbf232587043ed2901bce7f22610c0419751c065922b488431851041310ad659e4b23520e1772ab29dcdeb2002222a363f0c2b1c972b3efe1",
+		"3f721f208e6199fe903545abc26c837ce59ac6fa45733f1baaf0222f8b7acb0424814fcb5eecf6c1d38f06e9d0a6ccfbf85ae612ab8735dfdf9ce84c372a77c8f9e1c1e952c3a61b7567dd0693016af51d2745822663d0c2367e3f4f0bed827feecc2aaf98c949b5ed0d35c3f1023d64ad1407924288d366ea159f46287e61ac",
+		"b799b045a58c8d2b4334cf54b78260b45eec544f9f2fb5bd12fb603eaee70db7317bf807c406e26373922b7b8920fa29142703dd52bdf280084fb7ef69da78afdf80b3586395b433dc66cde048a258e476a561e9deba7060af40adf30c64249ca7ddea79806ee5beb9a1422949471d267b21bc88e688e4014087a0b592b695ed",
+		"05b0bfef265dcee87654372777b7c44177e2ae4c13a27f103340d9cd11c86cb2426ffcad5bd964080c2aee97f03be1ca18e30a1f14e27bc11ebbd650f305269cc9fb1db08bf90bfc79b42a952b46daf810359e7bc36452684784a64952c343c52e5124cd1f71d474d5197fefc571a92929c9084ffe1112cf5eea5192ebff330b",
+	}
 
 	// Short
 	for i := 0; i < len(inputs); i++ {
