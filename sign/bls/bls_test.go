848f428c4266d74be6ef30b25672bd52347b889f
Matteo Suez <mattsuez01@gmail.com>
2024-05-10 10:30:50 +0200

    Added fuzzer tests (#500)
    * Added fuzzer for tbls routine
    * Added fuzzers to some sign packages
    * Update README.md
    Add a more prominent disclaimer about target audience of this library.
    
    * Added fuzzers to some sign packages
    * Used deterministic streams in fuzzers
    * Some refactoring
    * Add test wrong shuffles on pair
    * Add test wrong Shuffle Sequence
    * Add negative test for shuffle/Biffle
    * Reduce code duplication
    * Added determinist stream to suite
    
    ---------
    
    Co-authored-by: Linus Gasser <linus.gasser@epfl.ch>
    Co-authored-by: lauener <kilian.lauener@epfl.ch>
    Co-authored-by: Yolan Romailler <AnomalRoil@users.noreply.github.com>

--- sign/bls/bls_test.go
+++ sign/bls/bls_test.go
@@ -1,129 +1,282 @@
 package bls
 
-/*func TestBLSBatchVerify(t *testing.T) {*/
-//msg1 := []byte("Hello Boneh-Lynn-Shacham")
-//msg2 := []byte("Hello Dedis & Boneh-Lynn-Shacham")
-//suite := bn256.NewSuite()
-//private1, public1 := NewKeyPair(suite, random.New())
-//private2, public2 := NewKeyPair(suite, random.New())
-//sig1, err := Sign(suite, private1, msg1)
-//require.Nil(t, err)
-//sig2, err := Sign(suite, private2, msg2)
-//require.Nil(t, err)
-//aggregatedSig, err := AggregateSignatures(suite, sig1, sig2)
-//require.Nil(t, err)
+import (
+	"crypto/rand"
+	"testing"
 
-//err = BatchVerify(suite, []kyber.Point{public1, public2}, [][]byte{msg1, msg2}, aggregatedSig)
-//require.Nil(t, err)
-//}
-//func TestBLSFailBatchVerify(t *testing.T) {
-//msg1 := []byte("Hello Boneh-Lynn-Shacham")
-//msg2 := []byte("Hello Dedis & Boneh-Lynn-Shacham")
-//suite := bn256.NewSuite()
-//private1, public1 := NewKeyPair(suite, random.New())
-//private2, public2 := NewKeyPair(suite, random.New())
-//sig1, err := Sign(suite, private1, msg1)
-//require.Nil(t, err)
-//sig2, err := Sign(suite, private2, msg2)
-//require.Nil(t, err)
+	"github.com/stretchr/testify/require"
+	"go.dedis.ch/kyber/v3"
+	"go.dedis.ch/kyber/v3/pairing/bn256"
+	"go.dedis.ch/kyber/v3/util/random"
+	"go.dedis.ch/kyber/v3/xof/blake2xb"
+)
 
-//t.Run("fails with a bad signature", func(t *testing.T) {
-//aggregatedSig, err := AggregateSignatures(suite, sig1, sig2)
-//require.Nil(t, err)
-//msg2[0] ^= 0x01
-//if BatchVerify(suite, []kyber.Point{public1, public2}, [][]byte{msg1, msg2}, aggregatedSig) == nil {
-//t.Fatal("bls: verification succeeded unexpectedly")
-//}
-//})
+func TestBLS(t *testing.T) {
+	suite := bn256.NewSuite()
+	msg := []byte("Hello Boneh-Lynn-Shacham")
+	BLSRoutine(t, msg, suite)
+}
 
-//t.Run("fails with a duplicate msg", func(t *testing.T) {
-//private3, public3 := NewKeyPair(suite, random.New())
-//sig3, err := Sign(suite, private3, msg1)
-//require.Nil(t, err)
-//aggregatedSig, err := AggregateSignatures(suite, sig1, sig2, sig3)
-//require.Nil(t, err)
+func FuzzBLS(f *testing.F) {
+	suite := bn256.NewSuite()
+	f.Fuzz(func(t *testing.T, msg []byte) {
+		if len(msg) < 1 || len(msg) > 1000 {
+			t.Skip("msg must have byte length between 1 and 1000")
+		}
+		BLSRoutine(t, msg, suite)
+	})
+}
 
-//if BatchVerify(suite, []kyber.Point{public1, public2, public3}, [][]byte{msg1, msg2, msg1}, aggregatedSig) == nil {
-//t.Fatal("bls: verification succeeded unexpectedly")
-//}
-//})
+func BLSRoutine(t *testing.T, msg []byte, suite *bn256.Suite) {
+	scheme := NewSchemeOnG1(suite)
+	private, public := scheme.NewKeyPair(blake2xb.New(msg))
+	sig, err := scheme.Sign(private, msg)
+	require.Nil(t, err)
+	err = scheme.Verify(public, msg, sig)
+	require.Nil(t, err)
+}
 
-//}
+func TestBLSFailSig(t *testing.T) {
+	msg := []byte("Hello Boneh-Lynn-Shacham")
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private, public := scheme.NewKeyPair(random.New())
+	sig, err := scheme.Sign(private, msg)
+	require.Nil(t, err)
+	sig[0] ^= 0x01
+	if scheme.Verify(public, msg, sig) == nil {
+		t.Fatal("bls: verification succeeded unexpectedly")
+	}
+}
 
-//func BenchmarkBLSKeyCreation(b *testing.B) {
-//suite := bn256.NewSuite()
-//b.ResetTimer()
-//for i := 0; i < b.N; i++ {
-//NewKeyPair(suite, random.New())
-//}
-//}
+func TestBLSFailKey(t *testing.T) {
+	msg := []byte("Hello Boneh-Lynn-Shacham")
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private, _ := scheme.NewKeyPair(random.New())
+	sig, err := scheme.Sign(private, msg)
+	require.Nil(t, err)
+	_, public := scheme.NewKeyPair(random.New())
+	if scheme.Verify(public, msg, sig) == nil {
+		t.Fatal("bls: verification succeeded unexpectedly")
+	}
+}
 
-//func BenchmarkBLSSign(b *testing.B) {
-//suite := bn256.NewSuite()
-//private, _ := NewKeyPair(suite, random.New())
-//msg := []byte("Hello many times Boneh-Lynn-Shacham")
-//b.ResetTimer()
-//for i := 0; i < b.N; i++ {
-//Sign(suite, private, msg)
-//}
-//}
+func TestBLSAggregateSignatures(t *testing.T) {
+	msg := []byte("Hello Boneh-Lynn-Shacham")
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private1, public1 := scheme.NewKeyPair(random.New())
+	private2, public2 := scheme.NewKeyPair(random.New())
+	sig1, err := scheme.Sign(private1, msg)
+	require.Nil(t, err)
+	sig2, err := scheme.Sign(private2, msg)
+	require.Nil(t, err)
+	aggregatedSig, err := scheme.AggregateSignatures(sig1, sig2)
+	require.Nil(t, err)
 
-//func BenchmarkBLSAggregateSigs(b *testing.B) {
-//suite := bn256.NewSuite()
-//private1, _ := NewKeyPair(suite, random.New())
-//private2, _ := NewKeyPair(suite, random.New())
-//msg := []byte("Hello many times Boneh-Lynn-Shacham")
-//sig1, err := Sign(suite, private1, msg)
-//require.Nil(b, err)
-//sig2, err := Sign(suite, private2, msg)
-//require.Nil(b, err)
+	aggregatedKey := scheme.AggregatePublicKeys(public1, public2)
 
-//b.ResetTimer()
-//for i := 0; i < b.N; i++ {
-//AggregateSignatures(suite, sig1, sig2)
-//}
-//}
+	err = scheme.Verify(aggregatedKey, msg, aggregatedSig)
+	require.Nil(t, err)
+}
 
-//func BenchmarkBLSVerifyAggregate(b *testing.B) {
-//suite := bn256.NewSuite()
-//private1, public1 := NewKeyPair(suite, random.New())
-//private2, public2 := NewKeyPair(suite, random.New())
-//msg := []byte("Hello many times Boneh-Lynn-Shacham")
-//sig1, err := Sign(suite, private1, msg)
-//require.Nil(b, err)
-//sig2, err := Sign(suite, private2, msg)
-//require.Nil(b, err)
-//sig, err := AggregateSignatures(suite, sig1, sig2)
-//key := AggregatePublicKeys(suite, public1, public2)
-//b.ResetTimer()
-//for i := 0; i < b.N; i++ {
-//Verify(suite, key, msg, sig)
-//}
-//}
+func TestBLSFailAggregatedSig(t *testing.T) {
+	msg := []byte("Hello Boneh-Lynn-Shacham")
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private1, public1 := scheme.NewKeyPair(random.New())
+	private2, public2 := scheme.NewKeyPair(random.New())
+	sig1, err := scheme.Sign(private1, msg)
+	require.Nil(t, err)
+	sig2, err := scheme.Sign(private2, msg)
+	require.Nil(t, err)
+	aggregatedSig, err := scheme.AggregateSignatures(sig1, sig2)
+	require.Nil(t, err)
+	aggregatedKey := scheme.AggregatePublicKeys(public1, public2)
 
-//func BenchmarkBLSVerifyBatchVerify(b *testing.B) {
-//suite := bn256.NewSuite()
+	aggregatedSig[0] ^= 0x01
+	if scheme.Verify(aggregatedKey, msg, aggregatedSig) == nil {
+		t.Fatal("bls: verification succeeded unexpectedly")
+	}
+}
+func TestBLSFailAggregatedKey(t *testing.T) {
+	msg := []byte("Hello Boneh-Lynn-Shacham")
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private1, public1 := scheme.NewKeyPair(random.New())
+	private2, public2 := scheme.NewKeyPair(random.New())
+	_, public3 := scheme.NewKeyPair(random.New())
+	sig1, err := scheme.Sign(private1, msg)
+	require.Nil(t, err)
+	sig2, err := scheme.Sign(private2, msg)
+	require.Nil(t, err)
+	aggregatedSig, err := scheme.AggregateSignatures(sig1, sig2)
+	require.Nil(t, err)
+	badAggregatedKey := scheme.AggregatePublicKeys(public1, public2, public3)
 
-//numSigs := 100
-//privates := make([]kyber.Scalar, numSigs)
-//publics := make([]kyber.Point, numSigs)
-//msgs := make([][]byte, numSigs)
-//sigs := make([][]byte, numSigs)
-//for i := 0; i < numSigs; i++ {
-//private, public := NewKeyPair(suite, random.New())
-//privates[i] = private
-//publics[i] = public
-//msg := make([]byte, 64, 64)
-//rand.Read(msg)
-//msgs[i] = msg
-//sig, err := Sign(suite, private, msg)
-//require.Nil(b, err)
-//sigs[i] = sig
-//}
+	if scheme.Verify(badAggregatedKey, msg, aggregatedSig) == nil {
+		t.Fatal("bls: verification succeeded unexpectedly")
+	}
+}
+func TestBLSBatchVerify(t *testing.T) {
+	msg1 := []byte("Hello Boneh-Lynn-Shacham")
+	msg2 := []byte("Hello Dedis & Boneh-Lynn-Shacham")
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private1, public1 := scheme.NewKeyPair(random.New())
+	private2, public2 := scheme.NewKeyPair(random.New())
+	sig1, err := scheme.Sign(private1, msg1)
+	require.Nil(t, err)
+	sig2, err := scheme.Sign(private2, msg2)
+	require.Nil(t, err)
+	aggregatedSig, err := scheme.AggregateSignatures(sig1, sig2)
+	require.Nil(t, err)
 
-//b.ResetTimer()
-//for i := 0; i < b.N; i++ {
-//aggregateSig, _ := AggregateSignatures(suite, sigs...)
-//BatchVerify(suite, publics, msgs, aggregateSig)
-//}
-//}
+	err = BatchVerify(suite, []kyber.Point{public1, public2}, [][]byte{msg1, msg2}, aggregatedSig)
+	require.Nil(t, err)
+}
+func TestBLSFailBatchVerify(t *testing.T) {
+	msg1 := []byte("Hello Boneh-Lynn-Shacham")
+	msg2 := []byte("Hello Dedis & Boneh-Lynn-Shacham")
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private1, public1 := scheme.NewKeyPair(random.New())
+	private2, public2 := scheme.NewKeyPair(random.New())
+	sig1, err := scheme.Sign(private1, msg1)
+	require.Nil(t, err)
+	sig2, err := scheme.Sign(private2, msg2)
+	require.Nil(t, err)
+
+	t.Run("fails with a bad signature", func(t *testing.T) {
+		aggregatedSig, err := scheme.AggregateSignatures(sig1, sig2)
+		require.Nil(t, err)
+		msg2[0] ^= 0x01
+		if BatchVerify(suite, []kyber.Point{public1, public2}, [][]byte{msg1, msg2}, aggregatedSig) == nil {
+			t.Fatal("bls: verification succeeded unexpectedly")
+		}
+	})
+
+	t.Run("fails with a duplicate msg", func(t *testing.T) {
+		private3, public3 := scheme.NewKeyPair(random.New())
+		sig3, err := scheme.Sign(private3, msg1)
+		require.Nil(t, err)
+		aggregatedSig, err := scheme.AggregateSignatures(sig1, sig2, sig3)
+		require.Nil(t, err)
+
+		if BatchVerify(suite, []kyber.Point{public1, public2, public3}, [][]byte{msg1, msg2, msg1}, aggregatedSig) == nil {
+			t.Fatal("bls: verification succeeded unexpectedly")
+		}
+	})
+
+}
+
+func BenchmarkBLSKeyCreation(b *testing.B) {
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		scheme.NewKeyPair(random.New())
+	}
+}
+
+func BenchmarkBLSSign(b *testing.B) {
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private, _ := scheme.NewKeyPair(random.New())
+	msg := []byte("Hello many times Boneh-Lynn-Shacham")
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		scheme.Sign(private, msg)
+	}
+}
+
+func BenchmarkBLSAggregateSigs(b *testing.B) {
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private1, _ := scheme.NewKeyPair(random.New())
+	private2, _ := scheme.NewKeyPair(random.New())
+	msg := []byte("Hello many times Boneh-Lynn-Shacham")
+	sig1, err := scheme.Sign(private1, msg)
+	require.Nil(b, err)
+	sig2, err := scheme.Sign(private2, msg)
+	require.Nil(b, err)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		scheme.AggregateSignatures(sig1, sig2)
+	}
+}
+
+func BenchmarkBLSVerifyAggregate(b *testing.B) {
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+	private1, public1 := scheme.NewKeyPair(random.New())
+	private2, public2 := scheme.NewKeyPair(random.New())
+	msg := []byte("Hello many times Boneh-Lynn-Shacham")
+	sig1, err := scheme.Sign(private1, msg)
+	require.Nil(b, err)
+	sig2, err := scheme.Sign(private2, msg)
+	require.Nil(b, err)
+	sig, err := scheme.AggregateSignatures(sig1, sig2)
+	key := scheme.AggregatePublicKeys(public1, public2)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		scheme.Verify(key, msg, sig)
+	}
+}
+
+func BenchmarkBLSVerifyBatchVerify(b *testing.B) {
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+
+	numSigs := 100
+	privates := make([]kyber.Scalar, numSigs)
+	publics := make([]kyber.Point, numSigs)
+	msgs := make([][]byte, numSigs)
+	sigs := make([][]byte, numSigs)
+	for i := 0; i < numSigs; i++ {
+		private, public := scheme.NewKeyPair(random.New())
+		privates[i] = private
+		publics[i] = public
+		msg := make([]byte, 64, 64)
+		rand.Read(msg)
+		msgs[i] = msg
+		sig, err := scheme.Sign(private, msg)
+		require.Nil(b, err)
+		sigs[i] = sig
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		aggregateSig, _ := scheme.AggregateSignatures(sigs...)
+		BatchVerify(suite, publics, msgs, aggregateSig)
+	}
+}
+
+func TestBinaryMarshalAfterAggregation_issue400(t *testing.T) {
+	suite := bn256.NewSuite()
+	scheme := NewSchemeOnG1(suite)
+
+	_, public1 := scheme.NewKeyPair(random.New())
+	_, public2 := scheme.NewKeyPair(random.New())
+
+	workingKey := scheme.AggregatePublicKeys(public1, public2, public1)
+
+	workingBits, err := workingKey.MarshalBinary()
+	require.Nil(t, err)
+
+	workingPoint := suite.G2().Point()
+	err = workingPoint.UnmarshalBinary(workingBits)
+	require.Nil(t, err)
+
+	// this was failing before the fix
+	aggregatedKey := scheme.AggregatePublicKeys(public1, public1, public2)
+
+	bits, err := aggregatedKey.MarshalBinary()
+	require.Nil(t, err)
+
+	point := suite.G2().Point()
+	err = point.UnmarshalBinary(bits)
+	require.Nil(t, err)
+}
