b17bebcd0bd08bd85bf77a1f6e39cf11a2c79666
Robin <robin.goumaz@epfl.ch>
2024-05-14 13:52:28 +0200

    Changed the path for test

--- sign/tbls/tbls_test.go
+++ sign/tbls/tbls_test.go
@@ -4,9 +4,9 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	"go.dedis.ch/kyber/v3/internal/test"
 	"go.dedis.ch/kyber/v3/pairing/bn256"
 	"go.dedis.ch/kyber/v3/share"
-	"go.dedis.ch/kyber/v3/sign/test"
 	"go.dedis.ch/kyber/v3/xof/blake2xb"
 )
 
