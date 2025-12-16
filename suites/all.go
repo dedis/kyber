185769710a52643751eaa5cb08ba59abb6ad03b6
HOT <theo@houle.pro>
2025-12-16 13:54:23 +0100

    remove cirl.BLS12381 from constant time suites

--- suites/all.go
+++ suites/all.go
@@ -4,11 +4,9 @@ package suites
 
 import (
 	"go.dedis.ch/kyber/v4/group/edwards25519"
-	"go.dedis.ch/kyber/v4/pairing/bls12381/circl"
 )
 
 func init() {
-	register(circl.NewSuiteBLS12381())
 	// This is a constant time implementation that should be
 	// used as much as possible
 	register(edwards25519.NewBlakeSHA256Ed25519())
